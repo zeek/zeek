7ba5d557d6c56e20ad3ea15098a887e708c44e9a
Hui Lin <hlin33@illinois.edu>
2011-07-13 23:56:04 -0500

    finishing testing event handler for Modbus

--- src/MODBUS.h
+++ src/MODBUS.h
@@ -20,7 +20,27 @@ public:
 
         // Put event names in this function
         static bool Available()
-                { return modbus_request; }
+                { return 
+		modbus_request || modbus_header || modbus_request_read_multiple_register || 
+		modbus_request_write_multiple_register || modbus_register_unit ||
+		modbus_request_read_coil || modbus_request_read_input_discrete || 
+		modbus_request_read_input_register || modbus_request_write_coil ||
+		modbus_request_write_single_register || modbus_request_read_exception_status ||
+		modbus_request_force_multiple_coils || modbus_request_read_general_reference ||
+		modbus_request_write_general_reference || modbus_reference ||
+		modbus_reference_with_data || modbus_register_value_unit || 
+		modbus_request_mask_write_register || modbus_request_read_write_registers ||
+		modbus_request_read_FIFO_queue ||
+		modbus_response || modbus_response_read_multiple_register ||
+		modbus_response_write_multiple_register || modbus_rregister_unit ||
+		modbus_response_read_coil || modbus_response_read_input_discrete ||
+		modbus_response_read_input_register || modbus_response_write_coil ||
+		modbus_response_write_single_register || modbus_response_read_exception_status ||
+		modbus_response_force_multiple_coils || modbus_response_read_general_reference ||
+		modbus_response_write_general_reference || modbus_response_mask_write_register ||
+		modbus_response_read_write_registers || modbus_response_read_FIFO_queue ||
+		modbus_rregister_data_unit || modbus_exception; 
+		}
 
 protected:
         binpac::ModbusTCP::ModbusTCP_Conn* interp;

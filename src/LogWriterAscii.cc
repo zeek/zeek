b05c34ed29c4bc6b48eb3ce5caca941bc75157a9
Jon Siwek <jsiwek@ncsa.illinois.edu>
2011-07-22 10:49:43 -0500

    LogWriterAscii now prints time values w/ constant 6 digit precision.
    closes #508

--- src/LogWriterAscii.cc
+++ src/LogWriterAscii.cc
@@ -141,8 +141,13 @@ bool LogWriterAscii::DoWriteOne(ODesc* desc, LogVal* val, const LogField* field)
 		desc->Add(dotted_addr(val->val.addr_val));
 		break;
 
-	case TYPE_DOUBLE:
 	case TYPE_TIME:
+		char buf[32];
+		snprintf(buf, sizeof(buf), "%.6f", val->val.double_val);
+		desc->Add(buf);
+		break;
+
+	case TYPE_DOUBLE:
 	case TYPE_INTERVAL:
 		desc->Add(val->val.double_val);
 	break;

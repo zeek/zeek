69ef268a4472f9dae1d50a39de38de934ef9cfb5
Robin Sommer <robin@icir.org>
2014-01-13 09:16:51 -0800

    Fixing compile problems with some versions of libc++.
    Reported by Craig Leres.

--- src/input/readers/Raw.cc
+++ src/input/readers/Raw.cc
@@ -365,7 +365,7 @@ bool Raw::DoInit(const ReaderInfo& info, int num_fields, const Field* const* fie
 		fname = source.substr(0, fname.length() - 1);
 		}
 
-	map<const char*, const char*>::const_iterator it = info.config.find("stdin"); // data that is sent to the child process
+	ReaderInfo::config_map::const_iterator it = info.config.find("stdin"); // data that is sent to the child process
 	if ( it != info.config.end() )
 		{
 		stdin_string = it->second;

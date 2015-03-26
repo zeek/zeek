be809b717166ccbf44fa264d9ceb05e2389f91db
Johanna Amann <johanna@icir.org>
2015-03-25 18:28:24 -0700

    src/3rdparty: update sqlite to 3.8.8.3

--- src/3rdparty/sqlite3.h
+++ src/3rdparty/sqlite3.h
@@ -107,9 +107,9 @@ extern "C" {
 ** [sqlite3_libversion_number()], [sqlite3_sourceid()],
 ** [sqlite_version()] and [sqlite_source_id()].
 */
-#define SQLITE_VERSION        "3.8.8.2"
+#define SQLITE_VERSION        "3.8.8.3"
 #define SQLITE_VERSION_NUMBER 3008008
-#define SQLITE_SOURCE_ID      "2015-01-30 14:30:45 7757fc721220e136620a89c9d28247f28bbbc098"
+#define SQLITE_SOURCE_ID      "2015-02-25 13:29:11 9d6c1880fb75660bbabd693175579529785f8a6b"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers

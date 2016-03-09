41fd96b3206b881bc14b2fc8d6fafc339dde5ee9
Johanna Amann <johanna@icir.org>
2016-03-09 09:23:50 -0800

    Fix the compile problem. Again. Now hopefully for all systems.

--- src/analyzer/protocol/finger/Finger.cc
+++ src/analyzer/protocol/finger/Finger.cc
@@ -3,7 +3,6 @@
 #include "bro-config.h"
 
 #include <ctype.h>
-#include <algorithm>
 
 #include "NetVar.h"
 #include "Finger.h"
@@ -56,7 +55,7 @@ void Finger_Analyzer::DeliverStream(int length, const u_char* data, bool is_orig
 			line = skip_whitespace(line+2, end_of_line);
 
 		assert(line <= end_of_line);
-		long n = std::min(end_of_line - line, 0L);	// just to be sure if assertions aren't on.
+		size_t n = end_of_line >= line ? end_of_line - line : 0; // just to be sure if assertions aren't on.
 		const char* at = reinterpret_cast<const char*>(memchr(line, '@', n));
 		const char* host = 0;
 		if ( ! at )

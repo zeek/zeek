ede0bad217a4c82512e9013dd24d9f4dd52c5b9a
Seth Hall <seth@icir.org>
2016-07-27 03:56:15 -0400

    Simplify how packets go into the SMB analyzer.
    There was a copy of every single packet being made as it entered
    the SMB analyzer.  This was because the entire analyzer used to be
    handwritten (only using binpac for unit parsing, not for parsing
    the whole protocol).  Now that the entire parser is binpac we don't
    need that layer of indirection anymore and we're able to remove
    a lot of complexity.

--- src/analyzer/protocol/smb/SMB.cc
+++ src/analyzer/protocol/smb/SMB.cc
@@ -10,90 +10,42 @@ SMB_Analyzer::SMB_Analyzer(Connection *conn)
 : tcp::TCP_ApplicationAnalyzer("SMB", conn)
 	{
 	chunks=0;
-	interp = new binpac::SMB::SMB_Conn(this);
-	AddSupportAnalyzer(new Contents_SMB(conn, true));
-	AddSupportAnalyzer(new Contents_SMB(conn, false));
+	interp=0;
 	}
 
 SMB_Analyzer::~SMB_Analyzer()
 	{
-	delete interp;
+	if ( interp )
+		delete interp;
 	}
 
 void SMB_Analyzer::Done()
 	{
 	TCP_ApplicationAnalyzer::Done();
 
-	interp->FlowEOF(true);
-	interp->FlowEOF(false);
+	if ( interp )
+		{
+		interp->FlowEOF(true);
+		interp->FlowEOF(false);
+		}
 	}
 
 void SMB_Analyzer::EndpointEOF(bool is_orig)
 	{
 	TCP_ApplicationAnalyzer::EndpointEOF(is_orig);
-	interp->FlowEOF(is_orig);
+
+	if ( interp )
+		interp->FlowEOF(is_orig);
 	}
 
 void SMB_Analyzer::Undelivered(uint64 seq, int len, bool orig)
 	{
 	TCP_ApplicationAnalyzer::Undelivered(seq, len, orig);
-	interp->NewGap(orig, len);
-	}
-
-void SMB_Analyzer::DeliverStream(int len, const u_char* data, bool orig)
-	{
-	TCP_ApplicationAnalyzer::DeliverStream(len, data, orig);
-
-	assert(TCP());
-
-	try
-		{
-		interp->NewData(orig, data, data + len);
-		// Let's assume that if there are no binpac exceptions after
-		// 3 data chunks that this is probably actually SMB.
-		if ( chunks >= 3 )
-			ProtocolConfirmation();
-		else
-			++chunks;
-		}
-	catch ( const binpac::Exception& e )
-		{
-		ProtocolViolation(fmt("Binpac exception: %s", e.c_msg()));
-		//printf(fmt("Binpac exception: %s", e.c_msg()));
-		}
-	}
-
-
-
-Contents_SMB::Contents_SMB(Connection* conn, bool orig)
-: TCP_SupportAnalyzer("Contents_SMB", conn, orig)
-	{
-	state = WAIT_FOR_HDR;
-	resync_state = INSYNC;
-	first_time = last_time = 0.0;
-	msg_len = 0;
-	msg_type = 0;
-	}
-
-void Contents_SMB::Init()
-	{
-	TCP_SupportAnalyzer::Init();
-
+	
 	NeedResync();
 	}
 
-Contents_SMB::~Contents_SMB()
-	{
-	}
-
-
-void Contents_SMB::Undelivered(uint64 seq, int len, bool orig)
-	{
-	TCP_SupportAnalyzer::Undelivered(seq, len, orig);
-	NeedResync();
-	}
-
-bool Contents_SMB::HasSMBHeader(int len, const u_char* data)
+bool SMB_Analyzer::HasSMBHeader(int len, const u_char* data)
 	{
 	if ( len < 8 )
 		return false;
@@ -102,94 +54,34 @@ bool Contents_SMB::HasSMBHeader(int len, const u_char* data)
 	        strncmp((const char*) data+4, "\xfeSMB", 4) == 0);
 	}
 
-void Contents_SMB::DeliverSMB(int len, const u_char* data)
+void SMB_Analyzer::DeliverStream(int len, const u_char* data, bool orig)
 	{
-	// Check the 4-byte header.
-	if ( ! HasSMBHeader(len, data) )
+	TCP_ApplicationAnalyzer::DeliverStream(len, data, orig);
+
+	assert(TCP());
+
+	// Either instantiate an interpreter or bail.
+	if ( ! interp )
 		{
-		if ( len >= 4 )
-			Conn()->Weird(fmt("SMB-over-TCP header error: %02x %05x, >>\\x%02x%c%c%c<<",
-				//dshdr[0], dshdr[1], dshdr[2], dshdr[3],
-				msg_type, msg_len,
-				data[0], data[1], data[2], data[3]));
+		if ( HasSMBHeader(len, data) )
+			interp = new binpac::SMB::SMB_Conn(this);
 		else
-			Conn()->Weird(fmt("SMB-over-TCP header error: %02x %05x", msg_type, msg_len));
+			return;
+		}
 
+	try
+		{
+		// If we get here, it means we have an interpreter.
+		interp->NewData(orig, data, data + len);
+
+		// Let's assume that if there are no binpac exceptions after
+		// 3 data chunks that this is probably actually SMB.
+		if ( ++chunks == 3 )
+			ProtocolConfirmation();
+		}
+	catch ( const binpac::Exception& e )
+		{
+		ProtocolViolation(fmt("Binpac exception: %s", e.c_msg()));
 		NeedResync();
 		}
-	else
-		{
-		ForwardStream(len, data, IsOrig());
-		}
-	}
-
-bool Contents_SMB::CheckResync(int& len, const u_char*& data, bool orig)
-	{
-	if (resync_state == INSYNC)
-		return true;
-
-	// This is an attempt to re-synchronize the stream after a content gap.
-	// Returns true if we are in sync.
-	// Returns false otherwise (we are in resync mode)
-	//
-	// We try to look for the beginning of a SMB message, assuming
-	// SMB messages start at packet boundaries (though they may span
-	// over multiple packets) (note that the data* of DeliverStream()
-	// usually starts at a packet boundrary).
-	//
-	// Now lets see whether data points to the beginning of a
-	// SMB message. If the resync processs is successful, we should
-	// be at the beginning of a frame.
-
-	// check if the SMB header starts with an SMB1 or SMB2 marker
-	if ( ! HasSMBHeader(len, data) )
-		{
-		NeedResync();
-		return false;
-		}
-
-	resync_state = INSYNC;
-	first_time = last_time = 0.0;
-	msg_len = 0;
-	msg_type = 0;
-	return true;
-	}
-
-void Contents_SMB::DeliverStream(int len, const u_char* data, bool orig)
-	{
-	TCP_SupportAnalyzer::DeliverStream(len, data, orig);
-
-	if (!CheckResync(len, data, orig))
-		return;   // Not in sync yet. Still resyncing
-
-	while ( len > 0 )
-		{
-		switch (state) {
-		case WAIT_FOR_HDR:
-			{
-			// We have the 4 bytes header now
-			msg_len = 0;
-			msg_type = data[0];
-			for ( int i = 1; i < 4; i++)
-				msg_len = (msg_len << 8) + data[i];
-			msg_len+=4;
-			msg_buf.Init(SMB_MAX_LEN+4, msg_len);
-			state = WAIT_FOR_DATA;
-			}
-			break;
-		case WAIT_FOR_DATA:
-			{
-			bool got_all_data = msg_buf.ConsumeChunk(data, len);
-			if ( got_all_data )
-				{
-				const u_char *dummy_p = msg_buf.GetBuf();
-				int dummy_len = (int) msg_buf.GetFill();
-				DeliverSMB(dummy_len, dummy_p);
-
-				state = WAIT_FOR_HDR;
-				}
-			}
-			break;
-		} // end switch
-		} // end while
 	}
--- src/analyzer/protocol/smb/SMB.h
+++ src/analyzer/protocol/smb/SMB.h
@@ -1,52 +1,10 @@
 #ifndef ANALYZER_PROTOCOL_SMB_SMB_H
 #define ANALYZER_PROTOCOL_SMB_SMB_H
 
-// SMB (CIFS) analyzer.
-// Reference: http://www.snia.org/tech_activities/CIFS/CIFS-TR-1p00_FINAL.pdf
-
 #include "analyzer/protocol/tcp/TCP.h"
-#include "analyzer/protocol/rpc/RPC.h"
 #include "smb_pac.h"
 
 namespace analyzer { namespace smb {
-
-class Contents_SMB : public tcp::TCP_SupportAnalyzer {
-public:
-	Contents_SMB(Connection* conn, bool orig);
-	~Contents_SMB();
-
-	void DeliverStream(int len, const u_char* data, bool orig) override;
-
-protected:
-	typedef enum {
-		WAIT_FOR_HDR,
-		WAIT_FOR_DATA
-	} state_t;
-	typedef enum {
-		NEED_RESYNC,
-		INSYNC,
-	} resync_state_t;
-	void Init() override;
-	virtual bool CheckResync(int& len, const u_char*& data, bool orig);
-	void Undelivered(uint64 seq, int len, bool orig) override;
-	virtual void NeedResync() {
-		resync_state = NEED_RESYNC;
-		state = WAIT_FOR_HDR;
-	}
-
-	bool HasSMBHeader(int len, const u_char* data);
-
-	void DeliverSMB(int len, const u_char* data);
-
-	rpc::RPC_Reasm_Buffer hdr_buf; // Reassembles the NetBIOS length and glue.
-	rpc::RPC_Reasm_Buffer msg_buf; // Reassembles the SMB message.
-	int msg_len;
-	int msg_type;
-	double first_time;   // timestamp of first packet of current message
-	double last_time;    // timestamp of last pakcet of current message
-	state_t state;
-	resync_state_t resync_state;
-};
 
 class SMB_Analyzer : public tcp::TCP_ApplicationAnalyzer {
 public:
@@ -57,6 +15,12 @@ public:
 	void DeliverStream(int len, const u_char* data, bool orig) override;
 	void Undelivered(uint64 seq, int len, bool orig) override;
 	void EndpointEOF(bool is_orig) override;
+
+	bool HasSMBHeader(int len, const u_char* data);
+	void NeedResync() {
+		delete interp;
+		interp = 0;
+	}
 
 	static analyzer::Analyzer* Instantiate(Connection* conn)
 		{ return new SMB_Analyzer(conn); }

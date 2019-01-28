5acafa0d340a6f4096dccbe69b8fb62d7c9ce87f
Jon Siwek <jsiwek@corelight.com>
2019-01-28 09:53:43 -0600

    Fix version in HTML title

--- conf.py
+++ conf.py
@@ -138,7 +138,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> Documentation".
-html_title = u'Zeek User Manual v' + release
+html_title = u'Zeek User Manual ' + release
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None

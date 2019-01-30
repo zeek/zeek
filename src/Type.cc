zeek/zeek

commit 298276512814bb358b14704c6e72340023fcb973

Pre-allocate and re-use Vals for bool, int, count, enum and empty string

src/Type.cc
@@ -1445,10 +1445,14 @@ EnumType::EnumType(EnumType* e)
 
 	for ( NameMap::iterator it = e->names.begin(); it != e->names.end(); ++it )
 		names[it->first] = it->second;
+
+	vals = e->vals;
 	}
 
 EnumType::~EnumType()
 	{
+	for ( auto& kv : vals )
+		Unref(kv.second);
 	}
 
 // Note, we use reporter->Error() here (not Error()) to include the current script
@@ -1524,6 +1528,9 @@ void EnumType::CheckAndAddName(const string& module_name, const char* name,
 
 	AddNameInternal(module_name, name, val, is_export);
 
+	if ( vals.find(val) == vals.end() )
+		vals[val] = new EnumVal(this, val);
+
 	set<BroType*> types = BroType::GetAliases(GetName());
 	set<BroType*>::const_iterator it;
 
@@ -1569,6 +1576,23 @@ EnumType::enum_name_list EnumType::Names() const
 		n.push_back(std::make_pair(iter->first, iter->second));
 
 	return n;
+	}
+
+EnumVal* EnumType::GetVal(bro_int_t i)
+	{
+	auto it = vals.find(i);
+	EnumVal* rval;
+
+	if ( it == vals.end() )
+		{
+		rval = new EnumVal(this, i);
+		vals[i] = rval;
+		}
+	else
+		rval = it->second;
+
+	::Ref(rval);
+	return rval;
 	}
 
 void EnumType::DescribeReST(ODesc* d, bool roles_only) const
@@ -1690,6 +1714,7 @@ bool EnumType::DoUnserialize(UnserialInfo* info)
 
 		names[name] = val;
 		delete [] name; // names[name] converts to std::string
+		vals[val] = new EnumVal(this, val);
 		}
 
 	return true;

commit 49a30d61cf17569164eba7964a226e35b09489ed

Fix memory leak due to enum type/val circular references
Introduced in 298276512814bb358b14704c6e72340023fcb

VERSION
@@ -1,1 +1,1 @@
-2.6-110
+2.6-111

src/Type.cc
@@ -1714,7 +1714,12 @@ bool EnumType::DoUnserialize(UnserialInfo* info)
 
 		names[name] = val;
 		delete [] name; // names[name] converts to std::string
-		vals[val] = new EnumVal(this, val);
+		// note: the 'vals' map gets populated lazily, which works fine and
+		// also happens to avoid a leak due to circular reference between the
+		// types and vals (there's a special case for unserializing a known
+		// type that will unserialze and then immediately want to unref the
+		// type if we already have it, except that won't delete it as intended
+		// if we've already created circular references to it here).
 		}
 
 	return true;

testing/btest/Baseline/core.leaks.broker.remote_event/send.send.out
@@ -1,4 +1,4 @@
-Broker peer added, [address=127.0.0.1, bound_port=9999/tcp]
+Broker peer added, 127.0.0.1
 got event msg, pong, 0
 got auto event msg, ping, 0
 got event msg, pong, 1

testing/btest/Baseline/core.leaks.broker.remote_log/send.send.out
@@ -1,1 +1,1 @@
-Broker peer added, [address=127.0.0.1, bound_port=9999/tcp]
+Broker peer added, 127.0.0.1

testing/external/commit-hash.zeek-testing
@@ -1,1 +1,1 @@
-42fd25c235d0c10319c3597d23f113dbbd89567f
+2f7a827cc339fc14b0c978c986fa776dd9e7903c

testing/external/commit-hash.zeek-testing-private
@@ -1,1 +1,1 @@
-e71c2dc19eabc68f590381f52a53a207f3b92f78
+60170dfea85dedd4dc9bece020d0614f04d7f30e
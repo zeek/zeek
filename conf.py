788e0ffe280bf750b1f3d0e687c427371165e0f3
Jon Siwek <jsiwek@corelight.com>
2019-04-25 10:06:51 -0700

    Add table cell wrapping
    The default Sphinx RTD theme doesn't wrap long table cell text and, for
    long tables, it's cumbersome to use the scroll bar, so this change adds
    wrapping.

--- conf.py
+++ conf.py
@@ -155,7 +155,10 @@ html_title = u'Zeek User Manual ' + release
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_stylesheet("theme_overrides.css")
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.

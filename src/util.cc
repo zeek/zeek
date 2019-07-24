ad19f1e1bbfefde19c56ef4f6998e9aa7f818b45
Tim Wojtulewicz <tim@corelight.com>
2019-07-24 10:38:45 -0700

    GHI-486: Switch over to using LLVM utf8-checking code to better validate characters

--- src/util.cc
+++ src/util.cc
@@ -51,6 +51,7 @@
 #include "Net.h"
 #include "Reporter.h"
 #include "iosource/Manager.h"
+#include "ConvertUTF.h"
 
 /**
  * Return IP address without enclosing brackets and any leading 0x.  Also
@@ -1889,8 +1890,9 @@ string json_escape_utf8(const string& val)
 	{
 	string result;
 	result.reserve(val.length());
+
+	auto val_data = reinterpret_cast<const unsigned char*>(val.c_str());
 	
-	size_t char_start = 0;
 	size_t idx;
 	for ( idx = 0; idx < val.length(); )
 		{
@@ -1910,50 +1912,23 @@ string json_escape_utf8(const string& val)
 			continue;
 			}
 
-		// The next bit is based on the table at https://en.wikipedia.org/wiki/UTF-8#Description.
-		// If next character is 11110xxx, this is a 4-byte UTF-8
-		unsigned int char_size = 0;
-		if ( (val[idx] & 0xF8) == 0xF0 ) char_size = 4;
-		
-		// If next character is 1110xxxx, this is a 3-byte UTF-8
-		else if ( (val[idx] & 0xF0) == 0xE0 ) char_size = 3;
-		
-		// If next character is 110xxxxx, this is a 2-byte UTF-8
-		else if ( (val[idx] & 0xE0) == 0xC0 ) char_size = 2;
-		
-		// This byte isn't a continuation byte, insert it as a byte and continue.
-		if ( char_size == 0)
+		// Find out how long the next character should be.
+		unsigned int char_size = getNumBytesForUTF8(val[idx]);
+
+		// If it says that it's a single character or it's not an invalid string UTF8 sequence, insert the one
+		// escaped byte into the string, step forward one, and go to the next character.
+		if ( char_size == 0 || isLegalUTF8Sequence(val_data+idx, val_data+idx+char_size) == 0 )
 			{
 			result.append(json_escape_byte(val[idx]));
 			++idx;
 			continue;
 			}
-		
-		// If we don't have enough bytes to get to the end of character, give up and insert all of the rest
-		// of them as escaped values.
-		if ( char_size > (val.length() - idx) )
-			break;
-		
-		// Loop through the rest of the supposed character and see if this is a valid character.
-		size_t c_idx = idx + 1;
-		for ( ; c_idx < idx + char_size; c_idx++ )
-			if ( (val[c_idx] & 0xC0) != 0x80 ) break;
-		
-		// if we didn't make it to the end of the character without finding an error, insert just this
-		// character and skip ahead. Otherwise insert all of the bytes for this character into the result.
-		if ( c_idx != idx + char_size )
-			{
-			result.append(json_escape_byte(val[idx]));
-			++idx;
-			continue;
-			}
-		else
-			{
-			for ( size_t step = 0; step < char_size; step++, idx++ )
-				result.push_back(val[idx]);
-			}
+
+		for ( size_t step = 0; step < char_size; step++, idx++ )
+			result.push_back(val[idx]);
 		}
-	
+
+	// Insert any of the remaining bytes into the string as escaped bytes
 	if ( idx != val.length() )
 		for ( ; idx < val.length(); ++idx )
 			result.append(json_escape_byte(val[idx]));

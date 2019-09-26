bc18ca44e6d18c28c92818fdc11dcd645c33d675
Jon Siwek <jsiwek@corelight.com>
2019-09-26 09:45:44 -0700

    Fix Xcode deprecation warning for std::ptr_fun
    Replaced logic in strstrip() with a lambda to avoid deprecations:
    
    - std::ptr_fun is deprecated in C++11, removed C++17
    - std::not1 is deprecated in C++17. removed C++20

--- src/util.cc
+++ src/util.cc
@@ -701,8 +701,9 @@ string strreplace(const string& s, const string& o, const string& n)
 
 std::string strstrip(std::string s)
 	{
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	auto notspace = [](unsigned char c) { return ! std::isspace(c); };
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), notspace));
+	s.erase(std::find_if(s.rbegin(), s.rend(), notspace).base(), s.end());
 	return s;
 	}
 

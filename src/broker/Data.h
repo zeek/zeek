86ac6fa1ac2202264a4ffb98847248e6482da5b2
Dominik Charousset <dominik.charousset@corelight.com>
2019-10-19 11:02:25 +0200

    Implement missing data_to_threading_val

--- src/broker/Data.h
+++ src/broker/Data.h
@@ -81,7 +81,7 @@ broker::data threading_field_to_data(const threading::Field* f);
  * @return a pointer to a new Bro threading::Value or a nullptr if the conversion was not
  * possible.
  */
-threading::Value* data_to_threading_val(broker::data d);
+threading::Value* data_to_threading_val(const broker::data& d);
 
 /**
  * Convert a Broker data value to a Bro threading::Value.

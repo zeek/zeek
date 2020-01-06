0cde6d2af522f63f1d24580c93923eb7bef5ffeb
Jon Siwek <jsiwek@corelight.com>
2020-01-06 10:51:07 -0800

    Fix Cirrus CI FreeBSD python dependencies installation

--- ci/freebsd/prepare.sh
+++ ci/freebsd/prepare.sh
@@ -6,6 +6,8 @@ set -e
 set -x
 
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
-pkg install -y bash git cmake swig30 bison python3 py36-sqlite3 py36-pip base64
+pkg install -y bash git cmake swig30 bison python3 base64
+pyver=`python3 -c 'import sys; print(f"py{sys.version_info[0]}{sys.version_info[1]}")'`
+pkg install -y $pyver-sqlite3 $pyver-pip
 ( cd && mkdir -p ./bin && ln -s /usr/local/bin/python3 ./bin/python )
 pip install junit2html

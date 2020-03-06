zeek/zeek

commit d81bfed45da04d0e77496ea4487832d5aa1f95ba

Fixing memory leaks in input framework.

src/input/Manager.cc
@@ -397,7 +397,9 @@ bool Manager::CreateEventStream(RecordVal* fval)
 	string stream_name = name_val->AsString()->CheckString();
 	Unref(name_val);
 
-	RecordType *fields = fval->Lookup("fields", true)->AsType()->AsTypeType()->Type()->AsRecordType();
+	Val* fields_val = fval->Lookup("fields", true);
+	RecordType *fields = fields_val->AsType()->AsTypeType()->Type()->AsRecordType();
+	Unref(fields_val);
 
 	Val *want_record = fval->Lookup("want_record", true);
 
@@ -548,13 +550,17 @@ bool Manager::CreateTableStream(RecordVal* fval)
 
 	Val* pred = fval->Lookup("pred", true);
 
-	RecordType *idx = fval->Lookup("idx", true)->AsType()->AsTypeType()->Type()->AsRecordType();
+	Val* idx_val = fval->Lookup("idx", true);
+	RecordType *idx = idx_val->AsType()->AsTypeType()->Type()->AsRecordType();
+	Unref(idx_val);
+
 	RecordType *val = 0;
 
-	if ( fval->Lookup("val", true) != 0 )
+	Val* val_val = fval->Lookup("val", true);
+	if ( val_val )
 		{
-		val = fval->Lookup("val", true)->AsType()->AsTypeType()->Type()->AsRecordType();
-		Unref(val); // The lookupwithdefault in the if-clause ref'ed val.
+		val = val_val->AsType()->AsTypeType()->Type()->AsRecordType();
+		Unref(val_val);
 		}
 
 	TableVal *dst = fval->Lookup("destination", true)->AsTableVal();
@@ -729,7 +735,7 @@ bool Manager::CreateTableStream(RecordVal* fval)
 	stream->pred = pred ? pred->AsFunc() : 0;
 	stream->num_idx_fields = idxfields;
 	stream->num_val_fields = valfields;
-	stream->tab = dst->AsTableVal();
+	stream->tab = dst->AsTableVal(); // ref'd by lookupwithdefault
 	stream->rtype = val ? val->AsRecordType() : 0;
 	stream->itype = idx->AsRecordType();
 	stream->event = event ? event_registry->Lookup(event->Name()) : 0;

VERSION
@@ -1,1 +1,1 @@
-2.2-137
+2.2-140

testing/btest/core/leaks/input-with-remove.bro
@@ -1,0 +1,63 @@
+# Needs perftools support.
+#
+# @TEST-GROUP: leaks
+#
+# @TEST-REQUIRES: bro  --help 2>&1 | grep -q mem-leaks
+#
+# @TEST-EXEC: HEAP_CHECK_DUMP_DIRECTORY=. HEAPCHECK=local btest-bg-run bro bro -b -m -r $TRACES/wikipedia.trace %INPUT
+# @TEST-EXEC: btest-bg-wait 15
+
+@load base/frameworks/input
+
+redef exit_only_after_terminate = T;
+
+global c: count = 0;
+
+
+type OneLine: record {
+	s: string;
+};
+
+event line(description: Input::EventDescription, tpe: Input::Event, s: string)
+	{
+	print "1", "Line";
+	}
+
+event InputRaw::process_finished(name: string, source:string, exit_code:count, signal_exit:bool)
+	{
+	Input::remove(name);
+	print "2", name;
+	}
+
+function run(): count
+	{
+	Input::add_event([$name=unique_id(""),
+	                  $source=fmt("%s |", "date"),
+	                  $reader=Input::READER_RAW,
+	                  $mode=Input::STREAM,
+	                  $fields=OneLine,
+	                  $ev=line,
+	                  $want_record=F]);
+
+	return 1;
+	}
+
+
+event do()
+       {
+       run();
+    	}
+
+event do_term() {
+	terminate();
+}
+
+event bro_init() {
+	schedule 1sec { 
+	 do() 
+	};
+	schedule 3sec { 
+	 do_term() 
+	};
+}
+

commit 4d39f53ab24099bca36b3ba4ed05ff5e46f23451

input/Manager: fix three use-after-free bugs
The code comment said "ref'd by lookupwithdefault", but the `fields`
variable was not referenced; only `fields_val` was, and its reference
was released earlier.

Same for `idx` and `val` in method CreateTableStream().

Fixes a regression from commit d81bfed45da04d0e77496ea4487832d5aa1f95ba

src/input/Manager.cc
@@ -467,7 +467,6 @@ bool Manager::CreateEventStream(RecordVal* fval)
 	for ( unsigned int i = 0; i < fieldsV.size(); i++ )
 		logf[i] = fieldsV[i];
 
-	Unref(fields); // ref'd by lookupwithdefault
 	stream->num_fields = fieldsV.size();
 	stream->fields = fields->Ref()->AsRecordType();
 	stream->event = event_registry->Lookup(event->Name());
@@ -511,7 +510,7 @@ bool Manager::CreateTableStream(RecordVal* fval)
 	Val* val_val = fval->Lookup("val", true);
 	if ( val_val )
 		{
-		val = val_val->AsType()->AsTypeType()->Type()->AsRecordType();
+		val = val_val->AsType()->AsTypeType()->Type()->Ref()->AsRecordType();
 		Unref(val_val);
 		}
 
@@ -712,7 +711,7 @@ bool Manager::CreateTableStream(RecordVal* fval)
 	stream->num_val_fields = valfields;
 	stream->tab = dst->AsTableVal(); // ref'd by lookupwithdefault
 	stream->rtype = val ? val->AsRecordType() : 0;
-	stream->itype = idx->AsRecordType();
+	stream->itype = idx->Ref()->AsRecordType();
 	stream->event = event ? event_registry->Lookup(event->Name()) : 0;
 	stream->error_event = error_event ? event_registry->Lookup(error_event->Name()) : nullptr;
 	stream->currDict = new PDict<InputHash>;
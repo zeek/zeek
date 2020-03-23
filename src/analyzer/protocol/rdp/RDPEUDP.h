97f777812dcd1b781a666bfa8337f75929a2f0c7
Anthony Kasza <ak@corelight.com>
2020-03-23 11:51:54 -0600

    add: analyzers initialization figured out

--- src/analyzer/protocol/rdp/RDPEUDP.h
+++ src/analyzer/protocol/rdp/RDPEUDP.h
@@ -5,23 +5,21 @@
 #include "rdpeudp_pac.h"
 
 namespace analyzer { namespace rdpeudp {
-
 class RDP_Analyzer : public analyzer::Analyzer {
 
 public:
 	explicit RDP_Analyzer(Connection* conn);
 	~RDP_Analyzer() override;
 
-	// Overriden from Analyzer.
 	void Done() override;
 	void DeliverPacket(int len, const u_char* data, bool orig,
 					uint64_t seq, const IP_Hdr* ip, int caplen) override;
 	void EndOfData(bool is_orig) override;
-	static analyzer::Analyzer* Instantiate(Connection* conn)
+	static analyzer::Analyzer* InstantiateAnalyzer(Connection* conn)
 		{ return new RDP_Analyzer(conn); }
 
 protected:
 	binpac::RDPEUDP::RDPEUDP_Conn* interp;
 };
 
-} } // namespace analyzer::* 
+} }

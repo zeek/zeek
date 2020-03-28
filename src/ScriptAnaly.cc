c27335bf7ab8057810ba1ddb60052ab5801f3ad9
Vern Paxson <vern@corelight.com>
2020-03-28 14:52:37 -0700

    factor out definition items

--- src/ScriptAnaly.cc
+++ src/ScriptAnaly.cc
@@ -1,6 +1,7 @@
 // See the file "COPYING" in the main distribution directory for copyright.
 
 #include "ScriptAnaly.h"
+#include "DefItem.h"
 #include "Desc.h"
 #include "Expr.h"
 #include "Stmt.h"
@@ -21,121 +22,6 @@ static const char* obj_desc(const BroObj* o)
 	strcpy(obj_desc_storage, d.Description());
 
 	return obj_desc_storage;
-	}
-
-
-class DefinitionItem {
-public:
-	DefinitionItem(const ID* _id)
-		{
-		is_id = true;
-		id = _id;
-		di = nullptr;
-		field_name = nullptr;
-		name = nullptr;
-
-		t = id->Type();
-
-		CheckForRecord();
-		}
-
-	DefinitionItem(const DefinitionItem* _di, const char* _field_name,
-			const BroType* _t)
-		{
-		is_id = false;
-		id = nullptr;
-		di = _di;
-		field_name = _field_name;
-
-		t = _t;
-
-		auto di_n = di->Name();
-		auto nl = strlen(di_n) + 1 /* $ */ + strlen(field_name) + 1;
-		name = new char[nl];
-		snprintf(name, nl, "%s$%s", di->Name(), field_name);
-
-		CheckForRecord();
-		}
-
-	~DefinitionItem()
-		{
-		if ( fields )
-			{
-			for ( int i = 0; i < num_fields; ++i )
-				delete fields[i];
-
-			delete fields;
-			}
-
-		delete name;
-		}
-
-	bool IsRecord() const	{ return t->Tag() == TYPE_RECORD; }
-
-	const char* Name() const
-		{
-		if ( name )
-			return name;
-		else
-			return id->Name();
-		}
-
-	const BroType* Type() const	{ return t; }
-
-	// For this definition item, look for a field corresponding
-	// to the given name.
-	DefinitionItem* FindField(const char* field) const;
-	DefinitionItem* CreateField(const char* field, const BroType* t);
-
-protected:
-	void CheckForRecord()
-		{
-		if ( ! IsRecord() )
-			{
-			rt = nullptr;
-			fields = nullptr;
-			return;
-			}
-
-		rt = t->AsRecordType();
-		num_fields = rt->NumFields();
-		fields = new DefinitionItem*[num_fields];
-
-		for ( int i = 0; i < num_fields; ++i )
-			fields[i] = nullptr;
-		}
-
-	bool is_id;
-	const ID* id;
-	const DefinitionItem* di;
-	const char* field_name;
-
-	const BroType* t;
-
-	char* name;
-
-	const RecordType* rt;
-	DefinitionItem** fields;	// indexed by field offset
-	int num_fields;
-};
-
-DefinitionItem* DefinitionItem::FindField(const char* field) const
-	{
-	if ( ! IsRecord() )
-		return nullptr;
-
-	auto offset = rt->FieldOffset(field);
-
-	return fields[offset];
-	}
-
-DefinitionItem* DefinitionItem::CreateField(const char* field, const BroType* t)
-	{
-	auto offset = rt->FieldOffset(field);
-
-	fields[offset] = new DefinitionItem(this, field, t);
-
-	return fields[offset];
 	}
 
 

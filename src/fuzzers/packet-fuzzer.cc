48ee85df0b0ebdd3fe53f3eee679215aa5241013
Justin Azoff <justin.azoff@gmail.com>
2020-05-03 16:58:40 -0400

    add packet fuzzer

---
+++ src/fuzzers/packet-fuzzer.cc
@@ -0,0 +1,50 @@
+#include "binpac.h"
+
+#include "Net.h"
+#include "Conn.h"
+#include "Sessions.h"
+#include "analyzer/Analyzer.h"
+#include "analyzer/Manager.h"
+#include "analyzer/protocol/pia/PIA.h"
+#include "analyzer/protocol/tcp/TCP.h"
+
+#include "FuzzBuffer.h"
+#include "fuzzer-setup.h"
+
+#include "pcap/dlt.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+	{
+
+	zeek::detail::FuzzBuffer fb{data, size};
+
+	if ( ! fb.Valid() )
+		return 0;
+
+	for ( ; ;  )
+		{
+		auto chunk = fb.Next();
+
+		if ( ! chunk )
+			break;
+
+		Packet pkt;
+		auto timestamp = current_time(true);
+		pkt_timeval ts = {long(timestamp), 0};
+		pkt.Init(DLT_RAW, &ts, chunk->size, chunk->size, chunk->data.get(), false, "");
+
+		try
+			{
+			sessions->NextPacket(timestamp, &pkt);
+			}
+		catch ( binpac::Exception const &e )
+			{
+			}
+
+		chunk = {};
+		mgr.Drain();
+		}
+
+	zeek::detail::fuzzer_cleanup_one_input();
+	return 0;
+	}

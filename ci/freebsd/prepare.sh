63a403f3d6f8c660528cd38b2c61d5ca32965eab
Jon Siwek <jsiwek@corelight.com>
2020-07-08 12:30:52 -0700

    Fix FreeBSD CI script to install right SWIG package
    (cherry picked from commit 3c1f5914c6d83e0e9a8a9eab7ae2f308170fe819)

--- ci/freebsd/prepare.sh
+++ ci/freebsd/prepare.sh
@@ -6,7 +6,7 @@ set -e
 set -x
 
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
-pkg install -y bash git cmake swig30 bison python3 base64
+pkg install -y bash git cmake swig bison python3 base64
 pyver=`python3 -c 'import sys; print(f"py{sys.version_info[0]}{sys.version_info[1]}")'`
 pkg install -y $pyver-sqlite3 $pyver-pip
 ( cd && mkdir -p ./bin && ln -s /usr/local/bin/python3 ./bin/python )

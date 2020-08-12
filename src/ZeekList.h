e78e68b2490fbec2975b7aa25cd4254131d72b07
Tim Wojtulewicz <tim@corelight.com>
2020-08-12 10:51:35 -0700

    Move ZeekList types to zeek namespace

--- src/ZeekList.h
+++ src/ZeekList.h
@@ -5,23 +5,30 @@
 #include "List.h"
 
 ZEEK_FORWARD_DECLARE_NAMESPACED(Val, zeek);
-using val_list = zeek::PList<zeek::Val>;
-
 ZEEK_FORWARD_DECLARE_NAMESPACED(Expr, zeek::detail);
-using expr_list = zeek::PList<zeek::detail::Expr>;
-
 ZEEK_FORWARD_DECLARE_NAMESPACED(ID, zeek::detail);
-using id_list = zeek::PList<zeek::detail::ID>;
-
 ZEEK_FORWARD_DECLARE_NAMESPACED(Stmt, zeek::detail);
-using stmt_list = zeek::PList<zeek::detail::Stmt>;
-
 namespace zeek { class Type; }
 using BroType [[deprecated("Remove in v4.1. Use zeek::Type instead.")]] = zeek::Type;
-using type_list = zeek::PList<zeek::Type>;
-
 ZEEK_FORWARD_DECLARE_NAMESPACED(Attr, zeek::detail);
-using attr_list = zeek::PList<zeek::detail::Attr>;
-
 ZEEK_FORWARD_DECLARE_NAMESPACED(Timer, zeek::detail);
-using timer_list = zeek::PList<zeek::detail::Timer, zeek::ListOrder::UNORDERED>;
+
+namespace zeek {
+
+using val_list = PList<Val>;
+using expr_list = PList<detail::Expr>;
+using id_list = PList<detail::ID>;
+using stmt_list = PList<detail::Stmt>;
+using type_list = PList<Type>;
+using attr_list = PList<detail::Attr>;
+using timer_list = PList<detail::Timer, ListOrder::UNORDERED>;
+
+} // namespace zeek
+
+using val_list [[deprecated("Remove in v4.1. Use zeek::val_list.")]] = zeek::val_list;
+using expr_list [[deprecated("Remove in v4.1. Use zeek::expr_list.")]] = zeek::expr_list;
+using id_list [[deprecated("Remove in v4.1. Use zeek::id_list.")]] = zeek::id_list;
+using stmt_list [[deprecated("Remove in v4.1. Use zeek::stmt_list.")]] = zeek::stmt_list;
+using type_list [[deprecated("Remove in v4.1. Use zeek::type_list.")]] = zeek::type_list;
+using attr_list [[deprecated("Remove in v4.1. Use zeek::attr_list.")]] = zeek::attr_list;
+using timer_list [[deprecated("Remove in v4.1. Use zeek::timer_list.")]] = zeek::timer_list;

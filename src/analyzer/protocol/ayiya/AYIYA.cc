22e1154a07c8ef3f337f77ffc1c0643fc31dd6f5
Jon Siwek <jsiwek@corelight.com>
2020-08-28 13:23:18 -0700

    Fix memory leak caused by re-entering AYIYA parsing
    If the inner packet of a decapsulated AYIYA frame maps to the same
    Connection as the outer packet and also contains another AYIYA
    encapsulation, the same AYIYA analyzer attempts to parse the inner
    packet.  In the process, the inner parsing method overwrites pointers to
    memory resources the outer parsing method has not yet released and
    results in a memory leak.
    
    Credit to OSS-Fuzz for discovery
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=25256

--- src/analyzer/protocol/ayiya/AYIYA.cc
+++ src/analyzer/protocol/ayiya/AYIYA.cc
@@ -33,4 +33,35 @@ void AYIYA_Analyzer::DeliverPacket(int len, const u_char* data, bool orig, uint6
 		{
 		ProtocolViolation(fmt("Binpac exception: %s", e.c_msg()));
 		}
+
+	if ( inner_packet_offset <= 0 )
+		return;
+
+	data += inner_packet_offset;
+	len -= inner_packet_offset;
+	caplen -= inner_packet_offset;
+	inner_packet_offset = -1;
+
+	IP_Hdr* inner = nullptr;
+	int result = sessions->ParseIPPacket(len, data, next_header, inner);
+
+	if ( result == 0 )
+		{
+		ProtocolConfirmation();
+		EncapsulatingConn ec(Conn(), BifEnum::Tunnel::AYIYA);
+		sessions->DoNextInnerPacket(network_time, nullptr,
+		                            inner, Conn()->GetEncapsulation(), ec);
+		}
+	else if ( result == -2 )
+		ProtocolViolation("AYIYA next header internal mismatch",
+		                  reinterpret_cast<const char*>(data), len);
+	else if ( result < 0 )
+		ProtocolViolation("Truncated AYIYA",
+		                  reinterpret_cast<const char*>(data), len);
+	else
+		ProtocolViolation("AYIYA payload length",
+		                  reinterpret_cast<const char*>(data), len);
+
+	if ( result != 0 )
+		delete inner;
 	}

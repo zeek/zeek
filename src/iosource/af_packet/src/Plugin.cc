62356c38ddaa35c6e2cac22d899438f1a2cb1a6f
Jan Grashoefer <jan.grashoefer@gmail.com>
2020-10-28 17:51:59 +0100

    af_packet: Fixed test baseline to include defrag option.

--- src/iosource/af_packet/src/Plugin.cc
+++ src/iosource/af_packet/src/Plugin.cc
@@ -16,6 +16,6 @@ plugin::Configuration Plugin::Configure()
 	config.description = "Packet acquisition via AF_Packet";
 	config.version.major = 2;
 	config.version.minor = 1;
-	config.version.patch = 0;
+	config.version.patch = 1;
 	return config;
 	}

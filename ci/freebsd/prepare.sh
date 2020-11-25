2b3d216c972ecf4d96163cdb55eaccff7139161e
Jon Siwek <jsiwek@corelight.com>
2020-11-24 20:35:31 -0800

    Update CI scripts to remove `python` vs `python3` workarounds

--- ci/freebsd/prepare.sh
+++ ci/freebsd/prepare.sh
@@ -9,5 +9,4 @@ env ASSUME_ALWAYS_YES=YES pkg bootstrap
 pkg install -y bash git cmake swig bison python3 base64
 pyver=`python3 -c 'import sys; print(f"py{sys.version_info[0]}{sys.version_info[1]}")'`
 pkg install -y $pyver-sqlite3 $pyver-pip
-( cd && mkdir -p ./bin && ln -s /usr/local/bin/python3 ./bin/python )
 pip install junit2html

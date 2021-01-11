28861a2867f854b07a151559eb24c7550799b134
Tim Wojtulewicz <tim@corelight.com>
2021-01-11 09:55:04 -0700

    Revert some files that shouldn't have changed

--- src/ConvertUTF.h
+++ src/ConvertUTF.h
@@ -146,10 +146,10 @@
     bit mask & shift operations.
 ------------------------------------------------------------------------ */
 
-typedef unsigned int UTF32; /* at least 32 bits */
-typedef unsigned short UTF16; /* at least 16 bits */
-typedef unsigned char UTF8; /* typically 8 bits */
-typedef unsigned char Boolean; /* 0 or 1 */
+typedef unsigned int    UTF32;  /* at least 32 bits */
+typedef unsigned short  UTF16;  /* at least 16 bits */
+typedef unsigned char   UTF8;   /* typically 8 bits */
+typedef unsigned char   Boolean; /* 0 or 1 */
 
 /* Some fundamental constants */
 #define UNI_REPLACEMENT_CHAR (UTF32)0x0000FFFD
@@ -160,75 +160,74 @@ typedef unsigned char Boolean; /* 0 or 1 */
 
 #define UNI_MAX_UTF8_BYTES_PER_CODE_POINT 4
 
-#define UNI_UTF16_BYTE_ORDER_MARK_NATIVE 0xFEFF
+#define UNI_UTF16_BYTE_ORDER_MARK_NATIVE  0xFEFF
 #define UNI_UTF16_BYTE_ORDER_MARK_SWAPPED 0xFFFE
 
-typedef enum
-{
-	conversionOK, /* conversion successful */
-	sourceExhausted, /* partial character in source, but hit end */
-	targetExhausted, /* insuff. room in target for conversion */
-	sourceIllegal /* source sequence is illegal/malformed */
+typedef enum {
+  conversionOK,           /* conversion successful */
+  sourceExhausted,        /* partial character in source, but hit end */
+  targetExhausted,        /* insuff. room in target for conversion */
+  sourceIllegal           /* source sequence is illegal/malformed */
 } ConversionResult;
 
-typedef enum
-{
-	strictConversion = 0,
-	lenientConversion
+typedef enum {
+  strictConversion = 0,
+  lenientConversion
 } ConversionFlags;
 
 /* This is for C++ and does no harm in C */
 #ifdef __cplusplus
-extern "C"
-	{
+extern "C" {
 #endif
 
-	ConversionResult ConvertUTF8toUTF16(const UTF8** sourceStart, const UTF8* sourceEnd,
-	                                    UTF16** targetStart, UTF16* targetEnd,
-	                                    ConversionFlags flags);
+ConversionResult ConvertUTF8toUTF16 (
+  const UTF8** sourceStart, const UTF8* sourceEnd,
+  UTF16** targetStart, UTF16* targetEnd, ConversionFlags flags);
 
-	/**
-	 * Convert a partial UTF8 sequence to UTF32.  If the sequence ends in an
-	 * incomplete code unit sequence, returns \c sourceExhausted.
-	 */
-	ConversionResult ConvertUTF8toUTF32Partial(const UTF8** sourceStart, const UTF8* sourceEnd,
-	                                           UTF32** targetStart, UTF32* targetEnd,
-	                                           ConversionFlags flags);
+/**
+ * Convert a partial UTF8 sequence to UTF32.  If the sequence ends in an
+ * incomplete code unit sequence, returns \c sourceExhausted.
+ */
+ConversionResult ConvertUTF8toUTF32Partial(
+  const UTF8** sourceStart, const UTF8* sourceEnd,
+  UTF32** targetStart, UTF32* targetEnd, ConversionFlags flags);
 
-	/**
-	 * Convert a partial UTF8 sequence to UTF32.  If the sequence ends in an
-	 * incomplete code unit sequence, returns \c sourceIllegal.
-	 */
-	ConversionResult ConvertUTF8toUTF32(const UTF8** sourceStart, const UTF8* sourceEnd,
-	                                    UTF32** targetStart, UTF32* targetEnd,
-	                                    ConversionFlags flags);
+/**
+ * Convert a partial UTF8 sequence to UTF32.  If the sequence ends in an
+ * incomplete code unit sequence, returns \c sourceIllegal.
+ */
+ConversionResult ConvertUTF8toUTF32(
+  const UTF8** sourceStart, const UTF8* sourceEnd,
+  UTF32** targetStart, UTF32* targetEnd, ConversionFlags flags);
 
-	/* NOTE: The source and end pointers must be aligned properly. */
-	ConversionResult ConvertUTF16toUTF8(const UTF16** sourceStart, const UTF16* sourceEnd,
-	                                    UTF8** targetStart, UTF8* targetEnd, ConversionFlags flags);
+/* NOTE: The source and end pointers must be aligned properly. */
+ConversionResult ConvertUTF16toUTF8 (
+  const UTF16** sourceStart, const UTF16* sourceEnd,
+  UTF8** targetStart, UTF8* targetEnd, ConversionFlags flags);
 
-	/* NOTE: The source and end pointers must be aligned properly. */
-	ConversionResult ConvertUTF32toUTF8(const UTF32** sourceStart, const UTF32* sourceEnd,
-	                                    UTF8** targetStart, UTF8* targetEnd, ConversionFlags flags);
+/* NOTE: The source and end pointers must be aligned properly. */
+ConversionResult ConvertUTF32toUTF8 (
+  const UTF32** sourceStart, const UTF32* sourceEnd,
+  UTF8** targetStart, UTF8* targetEnd, ConversionFlags flags);
 
-	/* NOTE: The source and end pointers must be aligned properly. */
-	ConversionResult ConvertUTF16toUTF32(const UTF16** sourceStart, const UTF16* sourceEnd,
-	                                     UTF32** targetStart, UTF32* targetEnd,
-	                                     ConversionFlags flags);
+/* NOTE: The source and end pointers must be aligned properly. */
+ConversionResult ConvertUTF16toUTF32 (
+  const UTF16** sourceStart, const UTF16* sourceEnd,
+  UTF32** targetStart, UTF32* targetEnd, ConversionFlags flags);
 
-	/* NOTE: The source and end pointers must be aligned properly. */
-	ConversionResult ConvertUTF32toUTF16(const UTF32** sourceStart, const UTF32* sourceEnd,
-	                                     UTF16** targetStart, UTF16* targetEnd,
-	                                     ConversionFlags flags);
+/* NOTE: The source and end pointers must be aligned properly. */
+ConversionResult ConvertUTF32toUTF16 (
+  const UTF32** sourceStart, const UTF32* sourceEnd,
+  UTF16** targetStart, UTF16* targetEnd, ConversionFlags flags);
 
-	Boolean isLegalUTF8Sequence(const UTF8* source, const UTF8* sourceEnd);
+Boolean isLegalUTF8Sequence(const UTF8 *source, const UTF8 *sourceEnd);
 
-	Boolean isLegalUTF8String(const UTF8** source, const UTF8* sourceEnd);
+Boolean isLegalUTF8String(const UTF8 **source, const UTF8 *sourceEnd);
 
-	unsigned getNumBytesForUTF8(UTF8 firstByte);
+unsigned getNumBytesForUTF8(UTF8 firstByte);
 
 #ifdef __cplusplus
-	}
+}
 #endif
 
-	/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */

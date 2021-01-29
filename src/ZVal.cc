4a91d6de623a498309ae3913a74563a8ea6ea5bd
Vern Paxson <vern@corelight.com>
2021-01-29 13:00:59 -0800

    port of basic code compiles

---
+++ src/ZVal.cc
@@ -0,0 +1,245 @@
+// See the file "COPYING" in the main distribution directory for copyright.
+
+#include "zeek/ZVal.h"
+
+#include "zeek/ZeekString.h"
+#include "zeek/File.h"
+#include "zeek/Func.h"
+#include "zeek/OpaqueVal.h"
+#include "zeek/Reporter.h"
+#include "zeek/Desc.h"
+
+using namespace zeek;
+
+
+bool ZVal::zval_error_status = false;
+
+
+bool zeek::IsManagedType(const TypePtr& t)
+	{
+	switch ( t->Tag() ) {
+	case zeek::TYPE_ADDR:
+	case zeek::TYPE_ANY:
+	case zeek::TYPE_FILE:
+	case zeek::TYPE_FUNC:
+	case zeek::TYPE_LIST:
+	case zeek::TYPE_OPAQUE:
+	case zeek::TYPE_PATTERN:
+	case zeek::TYPE_RECORD:
+	case zeek::TYPE_STRING:
+	case zeek::TYPE_SUBNET:
+	case zeek::TYPE_TABLE:
+	case zeek::TYPE_TYPE:
+	case zeek::TYPE_VECTOR:
+		return true;
+
+	default:
+		return false;
+
+	}
+	}
+
+
+ZVal::ZVal(ValPtr v, const TypePtr& t)
+	{
+	if ( ! v )
+		{
+		// This can happen for some forms of error propagation.
+		// We can deal with it iff the type is managed, and thus
+		// we can employ a "nil" placeholder.
+		ASSERT(IsManagedType(t));
+		managed_val = nullptr;
+		return;
+		}
+
+	auto vt = v->GetType();
+
+	if ( vt->Tag() != t->Tag() && t->Tag() != TYPE_ANY )
+		{
+		if ( t->InternalType() == TYPE_INTERNAL_OTHER ||
+		     t->InternalType() != vt->InternalType() )
+			reporter->InternalError("type inconsistency in ZVal constructor");
+		}
+
+	switch ( t->Tag() ) {
+	case TYPE_BOOL:
+	case TYPE_INT:
+	case TYPE_ENUM:
+		int_val = v->AsInt();
+		break;
+
+	case TYPE_COUNT:
+	case TYPE_PORT:
+		uint_val = v->AsCount();
+		break;
+
+	case TYPE_DOUBLE:
+	case TYPE_INTERVAL:
+	case TYPE_TIME:
+		double_val = v->AsDouble();
+		break;
+
+	case TYPE_FUNC:
+		func_val = v->AsFunc();
+		Ref(func_val);
+		break;
+
+	case TYPE_FILE:
+		file_val = v->AsFile();
+		Ref(file_val);
+		break;
+
+	case TYPE_LIST:
+		list_val = v.release()->AsListVal();
+		break;
+
+	case TYPE_OPAQUE:
+		opaque_val = v.release()->AsOpaqueVal();
+		break;
+
+	case TYPE_PATTERN:
+		re_val = v.release()->AsPatternVal();
+		break;
+
+	case TYPE_TABLE:
+		table_val = v.release()->AsTableVal();
+		break;
+
+	case TYPE_VECTOR:
+		{
+		vector_val = v.release()->AsVectorVal();
+
+		// Some run-time type-checking, sigh.
+		auto my_ytag = t->AsVectorType()->Yield()->Tag();
+		auto v_ytag = vt->AsVectorType()->Yield()->Tag();
+
+		if ( my_ytag != v_ytag && my_ytag != TYPE_ANY &&
+		     v_ytag != TYPE_ANY )
+			{
+			// Despite the above checks, this clash can still
+			// happen thanks to the intercession of vector-of-any,
+			// which for example can allow a function to return
+			// a concrete vector-of-X that's assigned to a local
+			// with a concrete vector-of-Y type.
+			reporter->Error("vector type clash: %s vs. %s (%s)",
+					type_name(my_ytag), type_name(v_ytag),
+					obj_desc(v.get()).c_str());
+			zval_error_status = true;
+			}
+
+		break;
+		}
+
+	case TYPE_RECORD:
+		record_val = v.release()->AsRecordVal();
+		break;
+
+	case TYPE_STRING:
+		string_val = v.release()->AsStringVal();
+		break;
+
+	case TYPE_ADDR:
+		addr_val = v.release()->AsAddrVal();
+		break;
+
+	case TYPE_SUBNET:
+		subnet_val = v.release()->AsSubNetVal();
+		break;
+
+	case TYPE_ANY:
+		any_val = v.release();
+		break;
+
+	case TYPE_TYPE:
+		type_val = t->Ref();
+		break;
+
+	case TYPE_ERROR:
+	case TYPE_TIMER:
+	case TYPE_UNION:
+	case TYPE_VOID:
+		reporter->InternalError("bad type in ZVal constructor");
+	}
+	}
+
+ValPtr ZVal::ToVal(const TypePtr& t) const
+	{
+	Val* v;
+
+	switch ( t->Tag() ) {
+	case TYPE_INT:
+		return val_mgr->Int(int_val);
+
+	case TYPE_BOOL:	
+		return val_mgr->Bool(int_val ? true : false);
+
+	case TYPE_PORT:
+		return val_mgr->Port(uint_val);
+
+	case TYPE_COUNT:
+		return val_mgr->Count(uint_val);
+
+	case TYPE_DOUBLE:
+		return make_intrusive<DoubleVal>(double_val);
+
+	case TYPE_INTERVAL:
+		return make_intrusive<IntervalVal>(double_val, Seconds);
+
+	case TYPE_TIME:
+		return make_intrusive<TimeVal>(double_val);
+
+	case TYPE_ENUM:
+		return t->AsEnumType()->GetEnumVal(int_val);
+
+	case TYPE_ANY:
+		return {NewRef{}, any_val};
+
+	case TYPE_TYPE:
+		{
+		TypePtr tp = {NewRef{}, type_val};
+		return make_intrusive<TypeVal>(tp);
+		}
+
+	case TYPE_FUNC:
+		if ( func_val )
+			{
+			FuncPtr fv_ptr = {NewRef{}, func_val};
+			return make_intrusive<FuncVal>(fv_ptr);
+			}
+
+		return nullptr;
+
+	case TYPE_FILE:
+		if ( file_val )
+			{
+			FilePtr fv_ptr = {NewRef{}, file_val};
+			return make_intrusive<FileVal>(fv_ptr);
+			}
+
+		return nullptr;
+
+	case TYPE_ADDR:		v = addr_val; break;
+	case TYPE_SUBNET:	v = subnet_val; break;
+	case TYPE_STRING:	v = string_val; break;
+	case TYPE_LIST:		v = list_val; break;
+	case TYPE_OPAQUE:	v = opaque_val; break;
+	case TYPE_TABLE:	v = table_val; break;
+	case TYPE_RECORD:	v = record_val; break;
+	case TYPE_VECTOR:	v = vector_val; break;
+	case TYPE_PATTERN:	v = re_val; break;
+
+	case TYPE_ERROR:
+	case TYPE_TIMER:
+	case TYPE_UNION:
+	case TYPE_VOID:
+		reporter->InternalError("bad ret type return tag");
+	}
+
+	if ( v )
+		return {NewRef{}, v};
+
+	reporter->Error("value used but not set");
+	zval_error_status = true;
+
+	return nullptr;
+	}
---
+++ src/ZVal.h
@@ -0,0 +1,97 @@
+// See the file "COPYING" in the main distribution directory for copyright.
+
+// Low-level representation of Zeek scripting values.
+
+#pragma once
+
+#include <unordered_set>
+
+#include "zeek/Dict.h"
+#include "zeek/Expr.h"
+
+namespace zeek {
+
+// Note that a ZVal by itself is ambiguous: it doesn't track its type.
+// This makes them consume less memory and cheaper to copy.  It does
+// however require a separate way to determine the type.  Generally
+// this is doable using surrounding context, or can be statically
+// determined in the case of optimization/compilation.
+//
+// An alternative would be to use std::variant, but it will be larger
+// due to needing to track the variant type, and it won't allow access
+// to the managed_val member, which both simplifies memory management
+// and is also required for sharing of ZAM frame slots.
+
+union ZVal {
+	// Constructor for hand-populating the values.
+	ZVal() { managed_val = nullptr; }
+
+	// Construct from a given higher-level script value with a given type.
+	ZVal(ValPtr v, const TypePtr& t);
+
+	// Convert to a higher-level script value.  The caller needs to
+	// ensure that they're providing the correct type.
+	ValPtr ToVal(const TypePtr& t) const;
+
+	// Whether a low-level ZVal error has occurred.  Used to generate
+	// run-time error messages.
+	static bool ZValErrorStatus()		{ return zval_error_status; }
+
+	// Resets the notion of low-level-error-occurred.
+	static void ClearZValErrorStatus()	{ zval_error_status = false; }
+
+private:
+	friend void DeleteManagedType(ZVal& v);
+
+	// Used for bool, int, enum.
+	bro_int_t int_val;
+
+	// Used for count and port.
+	bro_uint_t uint_val;
+
+	// Used for double, time, interval.
+	double double_val;
+
+	// The types are all variants of Val, Type, or more fundamentally
+	// Obj.  They are raw pointers rather than IntrusivePtr's because
+	// unions can't contain the latter.  For memory management, we use
+	// Ref/Unref.
+	StringVal* string_val;
+	AddrVal* addr_val;
+	SubNetVal* subnet_val;
+	File* file_val;
+	Func* func_val;
+	ListVal* list_val;
+	OpaqueVal* opaque_val;
+	PatternVal* re_val;
+	TableVal* table_val;
+	RecordVal* record_val;
+	VectorVal* vector_val;
+	Type* type_val;
+
+	// Used for "any" values.
+	Val* any_val;
+
+	// Used for generic access to managed (derived-from-Obj) objects.
+	Obj* managed_val;
+
+	// A class-wide status variable set to true when a run-time
+	// error associated with ZVal's occurs.  Static because often
+	// the caller won't have direct access to the particular ZVal
+	// that experienced the error, and just wants to know whether
+	// *some* error has occurred.
+	static bool zval_error_status;
+};
+
+// True if a given type is one for which we manage the associated
+// memory internally.
+bool IsManagedType(const TypePtr& t);
+
+// Deletes a managed value.  Caller needs to ensure that the ZVal
+// indeed holds such.
+inline void DeleteManagedType(ZVal& v)
+	{
+	Unref(v.managed_val);
+	}
+
+} // zeek

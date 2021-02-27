b581c6435e26507bb6b2c59f7df0b6cc3457fa5d
Vern Paxson <vern@corelight.com>
2021-02-27 11:08:34 -0800

    track more information about temporary variables

--- src/script_opt/TempVar.cc
+++ src/script_opt/TempVar.cc
@@ -11,7 +11,31 @@ TempVar::TempVar(int num, const TypePtr& t, ExprPtr _rhs) : type(t)
 	char buf[8192];
 	snprintf(buf, sizeof buf, "#%d", num);
 	name = buf;
-	id = nullptr;
+	rhs = _rhs;
+	}
+
+void TempVar::SetAlias(IDPtr _alias, const DefPoints* _dps)
+	{
+	if ( alias )
+		reporter->InternalError("Re-aliasing a temporary");
+
+	if ( ! _dps )
+		{
+		printf("trying to alias %s to %s\n", name.c_str(), _alias->Name());
+		reporter->InternalError("Empty dps for alias");
+		}
+
+	if ( alias == id )
+		reporter->InternalError("Creating alias loop");
+
+	alias = _alias;
+	dps = _dps;
+	}
+
+void TempVar::SetDPs(const DefPoints* _dps)
+	{
+	ASSERT(_dps->length() == 1);
+	dps = _dps;
 	}
 
 } // zeek::detail

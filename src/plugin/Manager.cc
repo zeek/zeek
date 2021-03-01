818deb98d2775dd6d923e5d70711d86a0b710593
Seth Hall <seth@corelight.com>
2021-03-01 12:12:36 -0500

    Minor nit change for Tim

--- src/plugin/Manager.cc
+++ src/plugin/Manager.cc
@@ -145,7 +145,7 @@ void Manager::LoadScriptsForStaticPlugins()
 	{
 	for ( const auto& p : Manager::ActivePlugins() )
 		{
-		if ( p->DynamicPlugin() || p->Name() == "" )
+		if ( p->DynamicPlugin() || p->Name().empty() )
 			continue;
 
 		string canon = std::regex_replace(p->Name(), std::regex("::"), "_");

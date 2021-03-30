dc0f0abc4b51a7020e81ff20f484b382d8dd3bb5
Andrew Benson <abenson@gmail.com>
2021-03-29 22:41:31 -0500

    Fix incomplete-type for struct timeval
    (cherry picked from commit 2ad482535ee36c99d7f9ea3bcd3077cb9c81bc78)

--- src/util.cc
+++ src/util.cc
@@ -5,17 +5,6 @@
 
 #include "zeek/util.h"
 
-#ifdef TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# ifdef HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
-#endif
-
 #ifdef HAVE_DARWIN
 #include <mach/task.h>
 #include <mach/mach_init.h>
--- src/util.h
+++ src/util.h
@@ -29,6 +29,17 @@
 #include <vector>
 #include <memory> // std::unique_ptr
 
+#ifdef TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
 #ifdef DEBUG
 
 #include <assert.h>

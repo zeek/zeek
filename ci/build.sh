cbd4c5ebf9710206753f029b383caadd7344497b
Tim Wojtulewicz <tim@corelight.com>
2021-06-23 13:09:00 -0700

    GH-1368: Use --osx-sysroot for macOS CI builds

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+# If we're on macOS, use --osx-sysroot to ensure we can find the SDKs from Xcode. This avoids
+# some problems with Catalina specifically, but it doesn't break anything on Big Sur either.
+if [ "${CIRRUS_OS}" == "darwin" ]; then
+    export ZEEK_CI_CONFIGURE_FLAGS="${ZEEK_CI_CONFIGURE_FLAGS} --osx-sysroot=$(xcrun --show-sdk-path)"
+fi
+
 if [ "${ZEEK_CI_CREATE_ARTIFACT}" != "1" ]; then
     ./configure ${ZEEK_CI_CONFIGURE_FLAGS}
     cd build

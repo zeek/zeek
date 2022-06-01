d8d300ac5270aa291ed2c4ca2cdc470b53ccfaf5
Tim Wojtulewicz <tim@corelight.com>
2022-06-01 12:47:29 -0700

    Merge remote-tracking branch 'origin/topic/timw/68-dns-edns0-hang'
    * origin/topic/timw/68-dns-edns0-hang:
      Fix hang in DNS analyzer with malformed packet
    
    (cherry picked from commit dc60633f7fe658fb8094055ce091e906c20065aa)

--- src/analyzer/protocol/dns/DNS.cc
+++ src/analyzer/protocol/dns/DNS.cc
@@ -251,6 +251,11 @@ bool DNS_Interpreter::ParseAnswer(detail::DNS_MsgInfo* msg,
 		analyzer->Weird("DNS_truncated_RR_rdlength_lt_len");
 		return false;
 		}
+	else if ( rdlength == 0 && len > 0 )
+		{
+		analyzer->Weird("DNS_zero_rdlength");
+		return false;
+		}
 
 	bool status;
 	switch ( msg->atype ) {
@@ -725,8 +730,9 @@ bool DNS_Interpreter::ParseRR_EDNS(detail::DNS_MsgInfo* msg,
 			msg->BuildEDNS_Val()
 		);
 
-	// parse EDNS options
-	while ( len > 0 )
+	// parse EDNS options. length has to be at least 4 to parse out the option
+	// code and length.
+	while ( len >= 4 )
 		{
 		uint16_t option_code = ExtractShort(data, len);
 		int option_len = ExtractShort(data, len);
@@ -932,6 +938,12 @@ bool DNS_Interpreter::ParseRR_EDNS(detail::DNS_MsgInfo* msg,
 				break;
 				}
 			}
+		}
+
+	if ( len > 0 )
+		{
+		analyzer->Weird("EDNS_truncated_option");
+		return false;
 		}
 
 	return true;

a927c4a998f2cefb136d9d1e712f6e441b3e6720
Tim Wojtulewicz <tim@corelight.com>
2022-08-24 08:28:00 -0700

    Switch macOS Monterey build to new m1-based VM

--- ci/macos/prepare.sh
+++ ci/macos/prepare.sh
@@ -9,3 +9,11 @@ brew update
 brew upgrade cmake openssl@1.1
 brew install swig bison flex ccache
 python3 -m pip install --user websockets
+
+# Brew doesn't create the /opt/homebrew/opt/openssl symlink if you install
+# openssl@1.1, only with 3.0. Create the symlink if it doesn't exist.
+if [ ! -e /opt/homebrew/opt/openssl ]; then
+    if [ -d /opt/homebrew/opt/openssl@1.1 ]; then
+        ln -s /opt/homebrew/opt/openssl@1.1 /opt/homebrew/opt/openssl
+    fi
+fi

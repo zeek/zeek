1061c051baf462e6115ab9699fb173eca62c8f8a
Tim Wojtulewicz <timwoj@gmail.com>
2022-09-14 17:59:59 +0000

    irc: Check parts.size() before removing the nick

--- src/analyzer/protocol/irc/IRC.cc
+++ src/analyzer/protocol/irc/IRC.cc
@@ -449,8 +449,6 @@ void IRC_Analyzer::DeliverStream(int length, const u_char* line, bool orig)
 			{
 			vector<string> parts = SplitWords(params, ' ');
 
-			// Remove nick name.
-			parts.erase(parts.begin());
 			if ( parts.size() < 2 )
 				{
 				Weird("irc_invalid_whois_channel_line");
@@ -458,6 +456,7 @@ void IRC_Analyzer::DeliverStream(int length, const u_char* line, bool orig)
 				}
 
 			string nick = parts[0];
+			// Remove nick name.
 			parts.erase(parts.begin());
 
 			if ( parts.size() > 0 && parts[0][0] == ':' )

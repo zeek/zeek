a7fd48928b491a3b8dbc383ed381df0900a56f99
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-23 16:03:25 -0400

    spelling: reassemble
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/packet_analysis/protocol/tcp/TCPSessionAdapter.cc
+++ src/packet_analysis/protocol/tcp/TCPSessionAdapter.cc
@@ -1602,7 +1602,7 @@ void TCPSessionAdapter::AddExtraAnalyzers(Connection* conn)
 	static zeek::Tag analyzer_connsize = analyzer_mgr->GetComponentTag("CONNSIZE");
 	static zeek::Tag analyzer_tcpstats = analyzer_mgr->GetComponentTag("TCPSTATS");
 
-	// We have to decide whether to reassamble the stream.
+	// We have to decide whether to reassemble the stream.
 	// We turn it on right away if we already have an app-layer
 	// analyzer, reassemble_first_packets is true, or the user
 	// asks us to do so.  In all other cases, reassembly may

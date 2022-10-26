165d60236c875501815f05018b852207b06d08e8
Jan Grashoefer <jan.grashoefer@corelight.com>
2022-10-26 21:19:54 +0200

    af_packet: Increase version number.

--- src/iosource/af_packet/src/Plugin.cc
+++ src/iosource/af_packet/src/Plugin.cc
@@ -14,8 +14,8 @@ zeek::plugin::Configuration Plugin::Configure()
 	zeek::plugin::Configuration config;
 	config.name = "Zeek::AF_Packet";
 	config.description = "Packet acquisition via AF_Packet";
-	config.version.major = 3;
-	config.version.minor = 2;
+	config.version.major = 4;
+	config.version.minor = 0;
 	config.version.patch = 0;
 	return config;
 	}

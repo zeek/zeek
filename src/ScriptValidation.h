3a99e60d1c4f44ee06a6aec709a3e2f5041abc49
Vern Paxson <vern@corelight.com>
2023-04-01 12:55:29 -0700

    extended script validation to be call-able on a per-function basis

--- src/ScriptValidation.h
+++ src/ScriptValidation.h
@@ -4,9 +4,17 @@
 namespace zeek::detail
 	{
 
+class Stmt;
+
 /**
  * Run extra validations on the parsed AST after everything is initialized
- * and report any errors via zeek::reporter->Error().
+ * and report any errors via deprecation warnings or zeek::reporter->Error().
  */
 void script_validation();
+
+/**
+ * Returns true if the given script statement (body) is valid.
+ */
+bool script_is_valid(const Stmt* s);
+
 	}

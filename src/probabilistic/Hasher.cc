4957dace641ecc650de07b9547fcbf57a7eded5a
Tim Wojtulewicz <timwoj@gmail.com>
2023-07-06 14:04:13 -0700

    Simplify type trait usage (remove ::value usage)

--- src/probabilistic/Hasher.cc
+++ src/probabilistic/Hasher.cc
@@ -108,7 +108,7 @@ UHF::UHF(Hasher::seed_t arg_seed)
 // times.
 Hasher::digest UHF::hash(const void* x, size_t n) const
 	{
-	static_assert(std::is_same<highwayhash::SipHashState::Key, decltype(seed.h)>::value,
+	static_assert(std::is_same_v<highwayhash::SipHashState::Key, decltype(seed.h)>,
 	              "Seed value is not the same type as highwayhash key");
 	return highwayhash::SipHash(seed.h, reinterpret_cast<const char*>(x), n);
 	}

c18963274dbd59f03c6a6e59d7bc1ce067fe6403
Vern Paxson <vern@corelight.com>
2023-07-18 15:53:43 -0700

    don't track flaky location

--- src/script_opt/Expr.cc
+++ src/script_opt/Expr.cc
@@ -1711,8 +1711,6 @@ ExprPtr AssignExpr::Reduce(Reducer* c, StmtPtr& red_stmt)
 
 	if ( lhs_is_any != rhs_is_any )
 		{
-		auto op2_loc = op2->GetLocationInfo();
-
 		ExprPtr red_rhs = op2->ReduceToSingleton(c, rhs_reduce);
 
 		if ( lhs_is_any )
@@ -1724,20 +1722,13 @@ ExprPtr AssignExpr::Reduce(Reducer* c, StmtPtr& red_stmt)
 			}
 		else
 			op2 = make_intrusive<CoerceFromAnyExpr>(red_rhs, t1);
-
-		if ( ! analysis_options.reduce_memory )
-			op2->SetLocationInfo(op2_loc);
 		}
 
 	if ( t1->Tag() == TYPE_VECTOR && t1->Yield()->Tag() != TYPE_ANY && t2->Yield() &&
 	     t2->Yield()->Tag() == TYPE_ANY )
 		{
-		auto op2_loc = op2->GetLocationInfo();
 		ExprPtr red_rhs = op2->ReduceToSingleton(c, rhs_reduce);
 		op2 = make_intrusive<CoerceFromAnyVecExpr>(red_rhs, t1);
-
-		if ( ! analysis_options.reduce_memory )
-			op2->SetLocationInfo(op2_loc);
 		}
 
 	auto lhs_ref = op1->AsRefExprPtr();

zeek/zeek

commit 2b9de839b0948c7de3eb5ed4a397194f96aae6b5

UDP: Forward any remaining data (also empty) to session-analysis
The protocol analyzers are prepared to receive truncated data and
this way we give analyzers a chance to look at data. We previously
allowed empty data being passed: When len ended up 0 and remaining
was 0 too.

src/packet_analysis/protocol/udp/UDP.cc
@@ -225,9 +225,8 @@ void UDPAnalyzer::DeliverPacket(Connection* c, double t, bool is_orig, int remai
 	// detection has to be used.
 	ForwardPacket(std::min(len, remaining), data, pkt, ntohs(c->RespPort()));
 
-	// Also try sending it into session analysis.
-	if ( remaining >= len )
-		adapter->ForwardPacket(len, data, is_orig, -1, ip.get(), remaining);
+	// Forward any data through session-analysis, too.
+	adapter->ForwardPacket(remaining, data, is_orig, -1, ip.get(), pkt->cap_len);
 	}
 
 bool UDPAnalyzer::ValidateChecksum(const IP_Hdr* ip, const udphdr* up, int len)

commit a391367c3664ea36345bc17831a10b21b8f5b177

Do not forward more than the remaining data to downstream UDP analyzer
This fixes a bug introduced in 2b9de839b0948c7de3eb5ed4a397194f96aae6b5
/ GH-3080, which causes UDP padding to be sent to UDP based analyzers.

Fixes GH-3205.

src/packet_analysis/protocol/udp/UDP.cc
@@ -226,7 +226,7 @@ void UDPAnalyzer::DeliverPacket(Connection* c, double t, bool is_orig, int remai
 	ForwardPacket(std::min(len, remaining), data, pkt, ntohs(c->RespPort()));
 
 	// Forward any data through session-analysis, too.
-	adapter->ForwardPacket(remaining, data, is_orig, -1, ip.get(), pkt->cap_len);
+	adapter->ForwardPacket(std::min(len, remaining), data, is_orig, -1, ip.get(), pkt->cap_len);
 	}
 
 bool UDPAnalyzer::ValidateChecksum(const IP_Hdr* ip, const udphdr* up, int len)

testing/btest/Baseline/core.udp-padding/syslog.log
@@ -1,0 +1,15 @@
+### BTest baseline data generated by btest-diff. Do not edit. Use "btest -U/-u" to update. Requires BTest >= 0.63.
+#separator \x09
+#set_separator	,
+#empty_field	(empty)
+#unset_field	-
+#path	syslog
+#open XXXX-XX-XX-XX-XX-XX
+#fields	ts	uid	id.orig_h	id.orig_p	id.resp_h	id.resp_p	proto	facility	severity	message
+#types	time	string	addr	port	addr	port	enum	string	string	string
+XXXXXXXXXX.XXXXXX	CHhAvVGS1DHFjwGM9	169.229.152.216	39887	192.150.187.42	514	udp	UNSPECIFIED	UNSPECIFIED	X
+XXXXXXXXXX.XXXXXX	CHhAvVGS1DHFjwGM9	169.229.152.216	39887	192.150.187.42	514	udp	UNSPECIFIED	UNSPECIFIED	X
+XXXXXXXXXX.XXXXXX	CHhAvVGS1DHFjwGM9	169.229.152.216	39887	192.150.187.42	514	udp	UNSPECIFIED	UNSPECIFIED	X
+XXXXXXXXXX.XXXXXX	CHhAvVGS1DHFjwGM9	169.229.152.216	39887	192.150.187.42	514	udp	UNSPECIFIED	UNSPECIFIED	X
+XXXXXXXXXX.XXXXXX	CHhAvVGS1DHFjwGM9	169.229.152.216	39887	192.150.187.42	514	udp	UNSPECIFIED	UNSPECIFIED	X
+#close XXXX-XX-XX-XX-XX-XX
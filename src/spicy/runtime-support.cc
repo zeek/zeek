d7c3e0ac69c21c81e90e2b497f25a031e1834b78
Benjamin Bannier <benjamin.bannier@corelight.com>
2023-09-06 08:19:38 +0200

    Propagate failure reason in `spicy::decline_input`/`zeek::reject_protocol`.
    Closes #3276.

--- src/spicy/runtime-support.cc
+++ src/spicy/runtime-support.cc
@@ -450,8 +450,8 @@ void rt::reject_protocol(const std::string& reason) {
 
     if ( auto x = cookie->protocol ) {
         auto tag = spicy_mgr->tagForProtocolAnalyzer(x->analyzer->GetAnalyzerTag());
-        SPICY_DEBUG(hilti::rt::fmt("rejecting protocol %s", tag.AsString()));
-        return x->analyzer->AnalyzerViolation("protocol rejected", nullptr, 0, tag);
+        SPICY_DEBUG(hilti::rt::fmt("rejecting protocol %s: %s", tag.AsString(), reason));
+        return x->analyzer->AnalyzerViolation(reason.c_str(), nullptr, 0, tag);
     }
     else
         throw ValueUnavailable("no current connection available");

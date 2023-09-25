80eaa4aa64cd2b99b1a3b9cabf88354cfecc17fc
Arne Welzel <arne.welzel@corelight.com>
2023-09-25 13:49:04 +0200

    cirrus/ci: Add ubuntu22_spicy_task and ubuntu22_spicy_head_task
    These tasks are meant to run nightly on the master branch. Currently,
    the external dns, http and dhcp Spicy analyzers are installed via zkg
    post building. The build artifact is uploaded to Cirrus and the benchmarker
    API triggered.
    
    For the spicy_head task, the auxil/spicy submodule is pulled to the latest
    commit. This also provides a bit of a nightly integration test.

---
+++ ci/spicy-install-analyzers.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+#
+# Shell script to install the latest version of certain
+# Spicy analyzers using zkg *and* repackages build.tgz.
+# This script should run after build.sh, but before the
+# artifact upload happens.
+set -eux
+
+test -d ${CIRRUS_WORKING_DIR}/install
+
+# Install prefix
+PREFIX=${CIRRUS_WORKING_DIR}/install
+
+export PATH=$PREFIX/bin:$PATH
+
+zkg --version
+
+ANALYZERS="
+https://github.com/zeek/spicy-dhcp
+https://github.com/zeek/spicy-dns
+https://github.com/zeek/spicy-http
+"
+
+for analyzer in $ANALYZERS; do
+    echo Y | zkg -vvvvv install "${analyzer}"
+done
+
+# After installing analyzers, package up build.tgz (representing
+# the contents of the installation directory). This overwrites any
+# existing artifact created by build.sh
+tar -czf ${CIRRUS_WORKING_DIR}/build.tgz ${CIRRUS_WORKING_DIR}/install

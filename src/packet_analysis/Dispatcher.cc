e56ef0fd9b8d998b56b58937c6ca44229e2d3de8
Arne Welzel <arne.welzel@corelight.com>
2023-11-01 10:03:49 +0100

    packet_analysis: Avoid shared_ptr copying for analyzer lookups
    For deeply encapsulated connections (think AWS traffic mirroring format
    like IP,UDP,GENEVE,IP,UDP,VXLAN,ETH,IP,TCP), the Dispatcher::Lookup()
    method is fairly visible in profiles when running in bare mode.
    
    This changes the Analyzer::Lookup() and Dispatcher::Lookup() return value
    breaking the API in favor of the performance improvement.
    
    Relates to zeek/zeek#3379.

--- src/packet_analysis/Dispatcher.cc
+++ src/packet_analysis/Dispatcher.cc
@@ -48,12 +48,12 @@ void Dispatcher::Register(uint32_t identifier, AnalyzerPtr analyzer) {
     table[index] = std::move(analyzer);
 }
 
-AnalyzerPtr Dispatcher::Lookup(uint32_t identifier) const {
+const AnalyzerPtr& Dispatcher::Lookup(uint32_t identifier) const {
     int64_t index = identifier - lowest_identifier;
     if ( index >= 0 && index < static_cast<int64_t>(table.size()) && table[index] != nullptr )
         return table[index];
 
-    return nullptr;
+    return Analyzer::nil;
 }
 
 size_t Dispatcher::Count() const {

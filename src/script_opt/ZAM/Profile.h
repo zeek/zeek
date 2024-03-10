8d762eea54c06de4b30236cad401a12a55931f6f
Vern Paxson <vern@corelight.com>
2024-03-10 13:31:14 -0700

    ZAM classes in support of finer-grained profiling

---
+++ src/script_opt/ZAM/Profile.h
@@ -0,0 +1,44 @@
+// See the file "COPYING" in the main distribution directory for copyright.
+
+// Classes for profiling ZAM execution.
+
+#pragma once
+
+#include "zeek/script_opt/ProfileFunc.h"
+#include "zeek/util.h"
+
+namespace zeek::detail {
+
+class ZAMLocInfo {
+public:
+    // A generalization of the notion of "Location" that includes associating
+    // with the location a function name, a static parent (i.e., one we can
+    // determine at compile time, reflecting an outer block or inlining), and
+    // a group of modules. It's a group of modules rather than a single one
+    // because of event handler coalescence.
+    ZAMLocInfo(std::string _func_name, std::shared_ptr<Location> _loc, std::shared_ptr<ZAMLocInfo> _parent);
+
+    const std::string& FuncName() const { return func_name; }
+    const Location* Loc() const { return loc.get(); }
+    std::shared_ptr<Location> LocPtr() const { return loc; }
+    std::shared_ptr<ZAMLocInfo> Parent() { return parent; }
+
+    // Add this location's modules to the target set.
+    void AddInModules(std::set<std::string>& target) const { target.insert(modules.begin(), modules.end()); }
+
+    // If include_lines is true, then in the description we include line
+    // number information, otherwise we omit them.
+    std::string Describe(bool include_lines) const;
+
+private:
+    std::string func_name;
+    std::set<std::string> modules;
+    std::shared_ptr<Location> loc;
+    std::shared_ptr<ZAMLocInfo> parent;
+};
+
+// Reports a profile of the different ZAM operations (instructions)
+// that executed.
+extern void report_ZOP_profile();
+
+} // namespace zeek::detail

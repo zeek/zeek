24916508730c1dc8373ec4cb3944c4083f1f023c
Tim Wojtulewicz <tim@corelight.com>
2024-04-08 10:47:24 -0700

    CI: Remove commented openssl 1.1 workaround from macOS preparation script

--- ci/macos/prepare.sh
+++ ci/macos/prepare.sh
@@ -14,11 +14,3 @@ if [ $(sw_vers -productVersion | cut -d '.' -f 1) -lt 14 ]; then
 fi
 
 python3 -m pip install --user --break-system-packages websockets
-
-# Brew doesn't create the /opt/homebrew/opt/openssl symlink if you install
-# openssl@1.1, only with 3.0. Create the symlink if it doesn't exist.
-#if [ ! -e /opt/homebrew/opt/openssl ]; then
-#    if [ -d /opt/homebrew/opt/openssl@1.1 ]; then
-#        ln -s /opt/homebrew/opt/openssl@1.1 /opt/homebrew/opt/openssl
-#    fi
-#fi

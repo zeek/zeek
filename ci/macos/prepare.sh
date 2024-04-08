167cbaf6e47e9f56e2475f3cb7782244ff261f05
Tim Wojtulewicz <tim@corelight.com>
2024-04-08 10:26:10 -0700

    CI: Fix installation of python package on macOS

--- ci/macos/prepare.sh
+++ ci/macos/prepare.sh
@@ -8,7 +8,12 @@ set -x
 brew update
 brew upgrade cmake
 brew install openssl@3 swig bison flex ccache libmaxminddb
-python3 -m pip install --user websockets
+
+if [ $(sw_vers -productVersion | cut -d '.' -f 1) -lt 14 ]; then
+    python3 -m pip install --upgrade pip
+fi
+
+python3 -m pip install --user --break-system-packages websockets
 
 # Brew doesn't create the /opt/homebrew/opt/openssl symlink if you install
 # openssl@1.1, only with 3.0. Create the symlink if it doesn't exist.

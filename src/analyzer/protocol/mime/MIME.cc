9a65a54ff53279814723940d53535cf7ef73dd60
Benjamin Bannier <benjamin.bannier@corelight.com>
2024-05-22 15:02:26 +0200

    Switch MIME mail content hashing to SHA1

--- src/analyzer/protocol/mime/MIME.cc
+++ src/analyzer/protocol/mime/MIME.cc
@@ -1165,7 +1165,7 @@ MIME_Mail::MIME_Mail(analyzer::Analyzer* mail_analyzer, bool orig, int buf_size)
 
     if ( mime_content_hash ) {
         compute_content_hash = 1;
-        md5_hash = zeek::detail::hash_init(detail::Hash_MD5);
+        sha1_hash = zeek::detail::hash_init(detail::Hash_SHA1);
     }
     else
         compute_content_hash = 0;
@@ -1182,12 +1182,12 @@ void MIME_Mail::Done() {
     SubmitAllData();
 
     if ( compute_content_hash && mime_content_hash ) {
-        u_char* digest = new u_char[16];
-        zeek::detail::hash_final(md5_hash, digest);
-        md5_hash = nullptr;
+        u_char* digest = new u_char[ZEEK_SHA_DIGEST_LENGTH];
+        zeek::detail::hash_final(sha1_hash, digest);
+        sha1_hash = nullptr;
 
         analyzer->EnqueueConnEvent(mime_content_hash, analyzer->ConnVal(), val_mgr->Count(content_hash_length),
-                                   make_intrusive<StringVal>(new String(true, digest, 16)));
+                                   make_intrusive<StringVal>(new String(true, digest, ZEEK_SHA_DIGEST_LENGTH)));
     }
 
     MIME_Message::Done();
@@ -1196,8 +1196,8 @@ void MIME_Mail::Done() {
 }
 
 MIME_Mail::~MIME_Mail() {
-    if ( md5_hash )
-        zeek::detail::hash_state_free(md5_hash);
+    if ( sha1_hash )
+        zeek::detail::hash_state_free(sha1_hash);
 
     delete_strings(all_content);
     delete data_buffer;
@@ -1257,7 +1257,7 @@ void MIME_Mail::SubmitData(int len, const char* buf) {
 
     if ( compute_content_hash ) {
         content_hash_length += len;
-        zeek::detail::hash_update(md5_hash, (const u_char*)buf, len);
+        zeek::detail::hash_update(sha1_hash, (const u_char*)buf, len);
     }
 
     if ( mime_entity_data || mime_all_data ) {

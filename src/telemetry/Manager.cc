777b0be03e4e62783910e205b21877286f66598e
Tim Wojtulewicz <tim@corelight.com>
2024-06-06 12:34:46 -0700

    Check for 'zeekctl check' before trying to start up prometheus

--- src/telemetry/Manager.cc
+++ src/telemetry/Manager.cc
@@ -70,16 +70,18 @@ void Manager::InitPostScript() {
             }
         }
 
-        try {
-            prometheus_exposer = std::make_unique<prometheus::Exposer>(prometheus_url, 2, callbacks);
+        if ( ! getenv("ZEEKCTL_CHECK_CONFIG") ) {
+            try {
+                prometheus_exposer = std::make_unique<prometheus::Exposer>(prometheus_url, 2, callbacks);
 
-            // CivetWeb stores a copy of the callbacks, so we're safe to delete the pointer here
-            delete callbacks;
-        } catch ( const CivetException& exc ) {
-            reporter->FatalError("Failed to setup Prometheus endpoint: %s\n", exc.what());
+                // CivetWeb stores a copy of the callbacks, so we're safe to delete the pointer here
+                delete callbacks;
+            } catch ( const CivetException& exc ) {
+                reporter->FatalError("Failed to setup Prometheus endpoint: %s\n", exc.what());
+            }
+
+            prometheus_exposer->RegisterCollectable(prometheus_registry);
         }
-
-        prometheus_exposer->RegisterCollectable(prometheus_registry);
     }
 
 #ifdef HAVE_PROCESS_STAT_METRICS

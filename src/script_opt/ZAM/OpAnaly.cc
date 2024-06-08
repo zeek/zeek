c9b1eea9463de32d3662cd68ac4c61604de60562
Vern Paxson <vern@corelight.com>
2024-06-08 09:28:58 -0700

    validation complete, other than some missing ops

--- src/script_opt/ZAM/OpAnaly.cc
+++ src/script_opt/ZAM/OpAnaly.cc
@@ -22,47 +22,47 @@ std::unordered_map<ZOp, ZAMInstDesc> zam_inst_desc = {
 };
 
 static std::map<char, string> type_pats = {
-    {'a', "any_val|AsAny\\(\\)"},
-    {'A', "addr_val|AsAddr\\(\\)"},
-    {'D', "double_val|AsDouble\\(\\)"},
-    {'F', "func_val|AsFunc\\(\\)"},
-    {'I', "int_val|AsInt\\(\\)"},
-    {'L', "list_val"},
-    {'N', "subnet_val|AsSubNet\\(\\)"},
-    {'P', "re_val|AsPattern\\(\\)"},
-    {'R', "record_val|AsRecord\\(\\)"},
-    {'S', "string_val|AsString\\(\\)"},
-    {'T', "table_val|AsTable\\(\\)"},
-    {'U', "uint_val|AsCount\\(\\)"},
-    {'V', "vector_val|AsVector(Ref)?\\(\\)"},
+    {'A', "addr_val|AsAddr\\(\\)"},     {'a', "any_val|AsAny\\(\\)"},
+    {'D', "double_val|AsDouble\\(\\)"}, {'F', "func_val|AsFunc\\(\\)"},
+    {'I', "int_val|AsInt\\(\\)"},       {'L', "list_val"},
+    {'N', "subnet_val|AsSubNet\\(\\)"}, {'P', "re_val|AsPattern\\(\\)"},
+    {'R', "record_val|AsRecord\\(\\)"}, {'S', "string_val|AsString\\(\\)"},
+    {'T', "table_val|AsTable\\(\\)"},   {'t', "type_val|AsType\\(\\)"},
+    {'U', "uint_val|AsCount\\(\\)"},    {'V', "vector_val|AsVector(Ref)?\\(\\)"},
 };
 
+int num_valid = 0;
+
 void analyze_ZAM_inst(const char* op_name, const ZAMInstDesc& zid) {
-    if ( ! zid.op_types.empty() ) {
-        auto& oc = zid.op_class;
-        auto& ot = zid.op_types;
-        auto& eval = zid.op_desc;
+    auto& oc = zid.op_class;
+    auto& ot = zid.op_types;
+    auto& eval = zid.op_desc;
 
-        if ( oc.size() != ot.size() )
-            reporter->InternalError("%s: instruction class/types mismatch (%s/%s)", op_name, oc.c_str(), ot.c_str());
+    bool have_ot = ! ot.empty();
 
-        int nslot = 0;
+    if ( have_ot && oc.size() != ot.size() )
+        reporter->InternalError("%s: instruction class/types mismatch (%s/%s)", op_name, oc.c_str(), ot.c_str());
 
-        for ( size_t i = 0; i < oc.size(); ++i ) {
-            string op;
+    int nslot = 0;
 
-            switch ( oc[i] ) {
-                case 'V': op = "frame\\[z\\.v" + std::to_string(++nslot) + "\\]"; break;
+    for ( size_t i = 0; i < oc.size(); ++i ) {
+        string op;
 
-                case 'i': op = "z\\.v" + std::to_string(++nslot); break;
+        switch ( oc[i] ) {
+            case 'V':
+            case 'R': op = "frame\\[z\\.v" + std::to_string(++nslot) + "\\]"; break;
 
-                case 'C': op = "z\\.c"; break;
+            case 'i': op = "z\\.v" + std::to_string(++nslot); break;
 
-                default:
-                    if ( ot[i] != 'X' )
-                        reporter->InternalError("instruction types mismatch: %s (%c)", op_name, oc[i]);
-            }
+            case 'C': op = "z\\.c"; break;
 
+            default:
+                if ( have_ot && ot[i] != 'X' )
+                    reporter->InternalError("instruction types mismatch: %s (%c)", op_name, oc[i]);
+        }
+
+        auto match_pat = op;
+        if ( have_ot ) {
             if ( ot[i] == 'X' || oc[i] == 'i' ) {
                 if ( ot[i] == 'X' && oc[i] == 'i' )
                     reporter->InternalError("empty instruction type for 'i' class element: %s", op_name);
@@ -76,17 +76,20 @@ void analyze_ZAM_inst(const char* op_name, const ZAMInstDesc& zid) {
             auto tp = type_pats.find(ot[i]);
             if ( tp == type_pats.end() )
                 reporter->InternalError("%s: instruction type %c not found", op_name, ot[i]);
-
-            auto match_pat = op + "." + string("(") + tp->second + ")";
-            if ( ! std::regex_search(eval, std::regex(match_pat)) )
-                reporter->InternalError("%s: did not find /%s/ in %s", op_name, match_pat.c_str(), eval.c_str());
+            match_pat += ".(" + tp->second + ")";
         }
+
+        if ( ! std::regex_search(eval, std::regex(match_pat)) )
+            reporter->InternalError("%s: did not find /%s/ in %s", op_name, match_pat.c_str(), eval.c_str());
     }
+    ++num_valid;
 }
 
 void analyze_ZAM_insts() {
     for ( auto& zid : zam_inst_desc )
         analyze_ZAM_inst(ZOP_name(zid.first), zid.second);
+
+    printf("%d valid\n", num_valid);
 }
 
 } // namespace zeek::detail

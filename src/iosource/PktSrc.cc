48249f85598243443e056617fe90d0ddc31ec39b
Arne Welzel <arne.welzel@corelight.com>
2024-08-29 21:06:55 +0200

    PktSrc: Header cleanup

--- src/iosource/PktSrc.cc
+++ src/iosource/PktSrc.cc
@@ -6,13 +6,11 @@
 
 #include <sys/stat.h>
 
-#include "zeek/Hash.h"
+#include "zeek/DebugLogger.h"
 #include "zeek/RunState.h"
-#include "zeek/broker/Manager.h"
 #include "zeek/iosource/BPF_Program.h"
 #include "zeek/iosource/Manager.h"
 #include "zeek/iosource/pcap/pcap.bif.h"
-#include "zeek/packet_analysis/Manager.h"
 #include "zeek/session/Manager.h"
 #include "zeek/util.h"
 

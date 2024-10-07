744628f115e6340fb120d020b054422707148780
Vern Paxson <vern@corelight.com>
2024-10-07 16:58:10 -0700

    factored CPP source's main header into collection of per-source-file headers

---
+++ src/script_opt/CPP/Attrs.h
@@ -0,0 +1,50 @@
+// See the file "COPYING" in the main distribution directory for copyright.
+
+// Methods for tracking attributes associated with Zeek variables/types.
+// Attributes arise mainly in the context of constructing types.
+//
+// This file is included by Compile.h to insert into the CPPCompiler class.
+
+public:
+// Tracks a use of the given set of attributes, including
+// initialization dependencies and the generation of any
+// associated expressions.
+//
+// Returns the initialization info associated with the set of
+// attributes.
+std::shared_ptr<CPP_InitInfo> RegisterAttributes(const AttributesPtr& attrs);
+
+// Convenient access to the global offset associated with
+// a set of Attributes.
+int AttributesOffset(const AttributesPtr& attrs) { return GI_Offset(RegisterAttributes(attrs)); }
+
+// The same, for a single attribute.
+std::shared_ptr<CPP_InitInfo> RegisterAttr(const AttrPtr& attr);
+
+// Returns a mapping of from Attr objects to their associated
+// initialization information.  The Attr must have previously
+// been registered.
+auto& ProcessedAttr() const { return processed_attr; }
+
+private:
+// Start of methods related to managing script type attributes.
+// Attributes arise mainly in the context of constructing types.
+// See Attrs.cc for definitions.
+//
+
+// Populates the 2nd and 3rd arguments with C++ representations
+// of the tags and (optional) values/expressions associated with
+// the set of attributes.
+void BuildAttrs(const AttributesPtr& attrs, std::string& attr_tags, std::string& attr_vals);
+
+// Returns a string representation of the name associated with
+// different attribute tags (e.g., "ATTR_DEFAULT").
+static const char* AttrName(AttrTag t);
+
+// Similar for attributes, so we can reconstruct record types.
+CPPTracker<Attributes> attributes = {"attrs", false};
+
+// Maps Attributes and Attr's to their global initialization
+// information.
+std::unordered_map<const Attributes*, std::shared_ptr<CPP_InitInfo>> processed_attrs;
+std::unordered_map<const Attr*, std::shared_ptr<CPP_InitInfo>> processed_attr;
---
+++ src/script_opt/CPP/Emit.h
@@ -0,0 +1,80 @@
+// See the file "COPYING" in the main distribution directory for copyright.
+
+// Low-level methods for emitting code.
+//
+// This file is included by Compile.h to insert into the CPPCompiler class.
+
+// The following all need to be able to emit code.
+friend class CPP_BasicConstInitsInfo;
+friend class CPP_CompoundInitsInfo;
+friend class IndicesManager;
+
+// Used to create (indented) C++ {...} code blocks.  "needs_semi"
+// controls whether to terminate the block with a ';' (such as
+// for class definitions.
+void StartBlock();
+void EndBlock(bool needs_semi = false);
+
+void IndentUp() { ++block_level; }
+void IndentDown() { --block_level; }
+
+// Various ways of generating code.  The multi-argument methods
+// assume that the first argument is a printf-style format
+// (but one that can only have %s specifiers).
+void Emit(const std::string& str) const {
+    Indent();
+    fprintf(write_file, "%s", str.c_str());
+    NL();
+}
+
+void Emit(const std::string& fmt, const std::string& arg, bool do_NL = true) const {
+    Indent();
+    fprintf(write_file, fmt.c_str(), arg.c_str());
+    if ( do_NL )
+        NL();
+}
+
+void Emit(const std::string& fmt, const std::string& arg1, const std::string& arg2) const {
+    Indent();
+    fprintf(write_file, fmt.c_str(), arg1.c_str(), arg2.c_str());
+    NL();
+}
+
+void Emit(const std::string& fmt, const std::string& arg1, const std::string& arg2, const std::string& arg3) const {
+    Indent();
+    fprintf(write_file, fmt.c_str(), arg1.c_str(), arg2.c_str(), arg3.c_str());
+    NL();
+}
+
+void Emit(const std::string& fmt, const std::string& arg1, const std::string& arg2, const std::string& arg3,
+          const std::string& arg4) const {
+    Indent();
+    fprintf(write_file, fmt.c_str(), arg1.c_str(), arg2.c_str(), arg3.c_str(), arg4.c_str());
+    NL();
+}
+
+void Emit(const std::string& fmt, const std::string& arg1, const std::string& arg2, const std::string& arg3,
+          const std::string& arg4, const std::string& arg5) const {
+    Indent();
+    fprintf(write_file, fmt.c_str(), arg1.c_str(), arg2.c_str(), arg3.c_str(), arg4.c_str(), arg5.c_str());
+    NL();
+}
+
+void Emit(const std::string& fmt, const std::string& arg1, const std::string& arg2, const std::string& arg3,
+          const std::string& arg4, const std::string& arg5, const std::string& arg6) const {
+    Indent();
+    fprintf(write_file, fmt.c_str(), arg1.c_str(), arg2.c_str(), arg3.c_str(), arg4.c_str(), arg5.c_str(),
+            arg6.c_str());
+    NL();
+}
+
+void NL() const { fputc('\n', write_file); }
+
+// Indents to the current indentation level.
+void Indent() const;
+
+// File to which we're generating code.
+FILE* write_file;
+
+// Indentation level.
+int block_level = 0;

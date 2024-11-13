6fb73aa9da9b0c24ea43df53eeb71f1df6afd34a
Arne Welzel <arne.welzel@corelight.com>
2024-11-13 12:49:28 +0100

    broker: Add shim plugin adding a backend component
    For broker, this isn't really functioning, but at least makes the
    CLUSTER_BACKEND_BROKER enum available.

---
+++ src/broker/Plugin.cc
@@ -0,0 +1,28 @@
+#include "zeek/broker/Plugin.h"
+
+#include <memory>
+
+#include "zeek/cluster/Backend.h"
+#include "zeek/cluster/Component.h"
+#include "zeek/cluster/Serializer.h"
+
+using namespace zeek::plugin::Zeek_Cluster_Backend_Broker;
+
+zeek::plugin::Configuration Plugin::Configure() {
+    // For now, there's always the broker_mgr instance that's explicitly
+    // instantiated in zeek-setup.cc. Don't even allow to instantiate
+    // a second one via the plugin mechanism. In the future, this could
+    // be changed so that broker is instantiated on demand only.
+    auto fail_instantiate = [](std::unique_ptr<cluster::EventSerializer>,
+                               std::unique_ptr<cluster::LogSerializer>) -> std::unique_ptr<cluster::Backend> {
+        zeek::reporter->FatalError("do not instantiate broker explicitly");
+        return nullptr;
+    };
+
+    AddComponent(new cluster::BackendComponent("BROKER", fail_instantiate));
+
+    zeek::plugin::Configuration config;
+    config.name = "Zeek::Cluster_Backend_Broker";
+    config.description = "Cluster backend using Broker";
+    return config;
+}

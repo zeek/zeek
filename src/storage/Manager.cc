f15d0403b526698a8fa7a9b8a778e6237f2f0845
Tim Wojtulewicz <tim@corelight.com>
2024-12-06 15:29:15 -0700

    Add infrastructure for automated expiration of storage entries
    This is used for backends that don't support expiration natively.

--- src/storage/Manager.cc
+++ src/storage/Manager.cc
@@ -2,11 +2,24 @@
 
 #include "zeek/storage/Manager.h"
 
+#include "const.bif.netvar_h"
+
 namespace zeek::storage {
+
+void detail::ExpireTimer::Dispatch(double t, bool is_expire) {
+    if ( is_expire )
+        return;
+
+    storage_mgr->Expire();
+    storage_mgr->StartExpireTimer();
+}
 
 Manager::Manager() : plugin::ComponentManager<storage::Component>("Storage", "Backend") {}
 
-void Manager::InitPostScript() { detail::backend_opaque = make_intrusive<OpaqueType>("Storage::Backend"); }
+void Manager::InitPostScript() {
+    detail::backend_opaque = make_intrusive<OpaqueType>("Storage::Backend");
+    StartExpireTimer();
+}
 
 BackendResult Manager::OpenBackend(const Tag& type, RecordValPtr config, TypePtr key_type, TypePtr val_type) {
     Component* c = Lookup(type);
@@ -36,20 +49,40 @@ BackendResult Manager::OpenBackend(const Tag& type, RecordValPtr config, TypePtr
     // TODO: post storage_connection_established event
 
     BackendPtr bp = IntrusivePtr<Backend>{AdoptRef{}, b};
-    backends.push_back(bp);
+
+    {
+        std::unique_lock<std::mutex> lk(backends_mtx);
+        backends.push_back(bp);
+    }
 
     return bp;
 }
 
 void Manager::CloseBackend(BackendPtr backend) {
-    auto it = std::find(backends.begin(), backends.end(), backend);
-    if ( it == backends.end() )
-        return;
+    {
+        std::unique_lock<std::mutex> lk(backends_mtx);
+        auto it = std::find(backends.begin(), backends.end(), backend);
+        if ( it == backends.end() )
+            return;
 
-    backends.erase(it);
+        backends.erase(it);
+    }
+
     backend->Done();
 
     // TODO: post storage_connection_lost event
+}
+
+void Manager::Expire() {
+    DBG_LOG(DBG_STORAGE, "Expire running, have %zu backends to check", backends.size());
+    std::unique_lock<std::mutex> lk(backends_mtx);
+    for ( const auto& b : backends )
+        b->Expire();
+}
+
+void Manager::StartExpireTimer() {
+    zeek::detail::timer_mgr->Add(
+        new detail::ExpireTimer(run_state::network_time + zeek::BifConst::Storage::expire_interval));
 }
 
 } // namespace zeek::storage

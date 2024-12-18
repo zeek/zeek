760d8786bfd44051ed4636365ab7b2c7c68f3db3
Tim Wojtulewicz <tim@corelight.com>
2024-12-18 10:28:31 -0700

    Redis: Add btests for the redis backend

--- src/storage/backends/redis/Redis.cc
+++ src/storage/backends/redis/Redis.cc
@@ -2,6 +2,7 @@
 
 #include "zeek/storage/backends/redis/Redis.h"
 
+#include "zeek/DebugLogger.h"
 #include "zeek/Func.h"
 #include "zeek/RunState.h"
 #include "zeek/Val.h"
@@ -93,6 +94,8 @@ ErrorResult Redis::DoOpen(RecordValPtr config) {
     // time moves forward based on the pcap and not based on real time.
     async_mode = config->GetField<BoolVal>("async_mode")->Get() && ! zeek::run_state::reading_traces;
     key_prefix = config->GetField<StringVal>("key_prefix")->ToStdString();
+
+    DBG_LOG(DBG_STORAGE, "Redis backend: running in async mode? %d", async_mode);
 
     redisOptions opt = {0};
 
@@ -338,6 +341,7 @@ void Redis::HandleEraseResult(redisReply* reply, ErrorResultCallback* callback) 
 }
 
 void Redis::OnConnect(int status) {
+    DBG_LOG(DBG_STORAGE, "Redis backend: connection event");
     if ( status == REDIS_OK ) {
         connected = true;
         return;
@@ -347,6 +351,7 @@ void Redis::OnConnect(int status) {
 }
 
 void Redis::OnDisconnect(int status) {
+    DBG_LOG(DBG_STORAGE, "Redis backend: disconnection event");
     if ( status == REDIS_OK ) {
         // TODO: this was an intentional disconnect, nothing to do?
     }

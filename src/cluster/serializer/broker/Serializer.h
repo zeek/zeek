273a6ec1f34cb2aaf9f9e4f953f5d3e2c882cb82
Arne Welzel <arne.welzel@corelight.com>
2025-01-09 20:51:48 +0100

    serializer/broker: Expose to_broker_event() and to_zeek_event()
    This is useful for reuse by WebSocket clients that use
    the JSON v1 encoding.

--- src/cluster/serializer/broker/Serializer.h
+++ src/cluster/serializer/broker/Serializer.h
@@ -4,7 +4,29 @@
 
 #include "zeek/cluster/Serializer.h"
 
+namespace broker::zeek {
+class Event;
+}
+
 namespace zeek::cluster::detail {
+
+/**
+ * Convert a broker::zeek::Event to cluster::detail::Event by looking
+ * it up in Zeek's event handler registry and converting event arguments
+ * to the appropriate Val instances.
+ *
+ * @param ev The broker side event.
+ * @returns A zeek::cluster::detail::Event instance, or std::nullopt if the conversion failed.
+ */
+std::optional<detail::Event> to_zeek_event(const broker::zeek::Event& ev);
+
+/**
+ * Convert a cluster::detail::Event to a broker::zeek::Event.
+ *
+ * @param ev The cluster::detail::Event
+ * @return A broker::zeek::Event to be serialized, or nullopt in case of errors.
+ */
+std::optional<broker::zeek::Event> to_broker_event(const detail::Event& ev);
 
 // Implementation of the EventSerializer using the existing broker::detail::val_to_data()
 // and broker::format::bin::v1::encode().

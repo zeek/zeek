e395332e7bdc19a23028e945ffbafc46e690f122
Arne Welzel <arne.welzel@corelight.com>
2025-01-28 14:17:53 +0100

    EventHandler: Support operator!=

--- src/EventHandler.h
+++ src/EventHandler.h
@@ -101,6 +101,8 @@ public:
 
     bool operator==(const EventHandlerPtr& h) const { return handler == h.handler; }
 
+    bool operator!=(const EventHandlerPtr& h) const { return ! (*this == h); }
+
     EventHandler* Ptr() { return handler; }
 
     explicit operator bool() const { return handler && *handler; }

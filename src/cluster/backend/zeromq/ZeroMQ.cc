6008e67008103dc1f23ed0015b1f0930def7ce99
Arne Welzel <arne.welzel@corelight.com>
2025-02-05 15:25:08 +0100

    cluster/zeromq: Call DoTerminate() in destructor
    Normal life-cycle is that Terminate() / DoTerminate() is called
    by zeek-setup code. If that doesn't happen, shutdown and join
    threads during destructor.
    
    try { } catch (...) suggested by Benjamin.

--- src/cluster/backend/zeromq/ZeroMQ.cc
+++ src/cluster/backend/zeromq/ZeroMQ.cc
@@ -66,6 +66,16 @@ ZeroMQBackend::ZeroMQBackend(std::unique_ptr<EventSerializer> es, std::unique_pt
     main_inproc = zmq::socket_t(ctx, zmq::socket_type::pair);
 }
 
+ZeroMQBackend::~ZeroMQBackend() {
+    try {
+        // DoTerminate is idempotent.
+        DoTerminate();
+    } catch ( ... ) {
+        // This should never happen.
+        abort();
+    }
+}
+
 void ZeroMQBackend::DoInitPostScript() {
     ThreadedBackend::DoInitPostScript();
 
@@ -109,6 +119,7 @@ void ZeroMQBackend::DoTerminate() {
     if ( proxy_thread ) {
         ZEROMQ_DEBUG("Shutting down proxy thread");
         proxy_thread->Shutdown();
+        proxy_thread.reset();
     }
 
     ZEROMQ_DEBUG("Terminated");

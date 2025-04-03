ec89d6ffff6ccf35e696bdb0e458d6763278eea0
Christian Kreibich <christian@corelight.com>
2025-04-02 18:08:30 -0700

    Make Zeekygen docs generation (-X) imply parse-only (-a)
    Nobody generates docs at startup while then moving on to regular Zeek operation,
    and the generated runtime output when loading scripts/zeekygen is both noisy and
    confusing.

--- src/zeek-setup.cc
+++ src/zeek-setup.cc
@@ -641,6 +641,10 @@ SetupResult setup(int argc, char** argv, Options* zopts) {
     auto zeekygen_cfg = options.zeekygen_config_file.value_or("");
     zeekygen_mgr = new zeekygen::detail::Manager(zeekygen_cfg, zeek_argv[0]);
 
+    // Zeekygen documentation generation (via -X) implies we only parse:
+    if ( ! zeekygen_cfg.empty() )
+        detail::parse_only = true;
+
     add_essential_input_file("base/init-bare.zeek");
     add_essential_input_file("builtin-plugins/__preload__.zeek");
     add_essential_input_file("base/init-frameworks-and-bifs.zeek");
@@ -985,7 +989,7 @@ SetupResult setup(int argc, char** argv, Options* zopts) {
         }
     }
 
-    if ( options.parse_only ) {
+    if ( detail::parse_only ) {
         if ( analysis_options.usage_issues > 0 )
             analyze_scripts(options.no_unused_warnings);
 

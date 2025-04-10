e246b6e88a01d08045a1c697c6a72a8bd2e0efaa
Tim Wojtulewicz <tim@corelight.com>
2025-04-10 16:12:32 -0700

    Replace includes of util.h where only zeek_int types are needed

--- src/Frag.h
+++ src/Frag.h
@@ -8,7 +8,7 @@
 #include "zeek/IPAddr.h"
 #include "zeek/Reassem.h"
 #include "zeek/Timer.h"
-#include "zeek/util.h" // for zeek_uint_t
+#include "zeek/util-types.h" // for zeek_uint_t
 
 namespace zeek {
 

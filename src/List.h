6417990222f3ebee750ea7b8a8ef6e0e06b19792
Tim Wojtulewicz <tim@corelight.com>
2025-04-11 16:38:59 -0700

    Use C++ version of standard headers

--- src/List.h
+++ src/List.h
@@ -21,8 +21,8 @@
 //	Entries must be either a pointer to the data or nonzero data with
 //	sizeof(data) <= sizeof(void*).
 
-#include <stdarg.h>
 #include <cassert>
+#include <cstdarg>
 #include <initializer_list>
 #include <iterator>
 

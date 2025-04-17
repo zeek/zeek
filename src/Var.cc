637a7732c0a22e00cf83aff09d5d31a82e252553
Tim Wojtulewicz <tim@corelight.com>
2025-04-17 15:53:19 -0700

    Fix clang-tidy bugprone-unused-return-value warnings

--- src/Var.cc
+++ src/Var.cc
@@ -750,6 +750,7 @@ void end_func(StmtPtr body, const char* module_name, bool free_of_conditionals) 
     // Note: ideally, something would take ownership of this memory until the
     // end of script execution, but that's essentially the same as the
     // lifetime of the process at the moment, so ok to "leak" it.
+    // NOLINTNEXTLINE(bugprone-unused-return-value)
     ingredients.release();
 }
 

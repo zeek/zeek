viyer-research/ZeekLocal

cluster/Backend: Add ProcessError()
Allow backends to pass errors to a strategy. Locally, these raise
Cluster::Backend::error() events that are logged to the reporter
as errors.

scripts/base/frameworks/cluster/main.zeek
@@ -667,3 +667,10 @@ event websocket_client_lost(endpoint: EndpointInfo)
 	                endpoint$id, endpoint$network$address, endpoint$network$bound_port);
 	Cluster::log(msg);
 	}
+
+# If a backend reports an error, propagate it via a reporter error message.
+event Cluster::Backend::error(code: string, message: string)
+	{
+	local msg = fmt("Cluster::Backend::error: %s (%s)", code, message);
+	Reporter::error(msg);
+	}

src/cluster/Backend.cc
@@ -12,6 +12,7 @@
 #include "zeek/Type.h"
 #include "zeek/cluster/OnLoop.h"
 #include "zeek/cluster/Serializer.h"
+#include "zeek/cluster/events.bif.h"
 #include "zeek/logging/Manager.h"
 #include "zeek/util.h"
 
@@ -25,6 +26,13 @@ bool detail::LocalEventHandlingStrategy::DoProcessEvent(std::string_view topic, 
 
 void detail::LocalEventHandlingStrategy::DoProcessLocalEvent(EventHandlerPtr h, zeek::Args args) {
     zeek::event_mgr.Enqueue(h, std::move(args));
+}
+
+// Backend errors are raised via a generic Cluster::Backend::error(code, message) event.
+void detail::LocalEventHandlingStrategy::DoProcessError(std::string_view code, std::string_view message) {
+    if ( Cluster::Backend::error )
+        zeek::event_mgr.Enqueue(Cluster::Backend::error, zeek::make_intrusive<zeek::StringVal>(code),
+                                zeek::make_intrusive<zeek::StringVal>(message));
 }
 
 std::optional<zeek::Args> detail::check_args(const zeek::FuncValPtr& handler, zeek::ArgsSpan args) {
@@ -117,6 +125,10 @@ void Backend::EnqueueEvent(EventHandlerPtr h, zeek::Args args) {
 
 bool Backend::ProcessEvent(std::string_view topic, detail::Event e) {
     return event_handling_strategy->ProcessEvent(topic, std::move(e));
+}
+
+void Backend::ProcessError(std::string_view code, std::string_view message) {
+    return event_handling_strategy->ProcessError(code, message);
 }
 
 bool Backend::ProcessEventMessage(std::string_view topic, std::string_view format,

src/cluster/Backend.h
@@ -89,11 +89,19 @@ public:
      * When the backend is instantiated for a WebSocket client,
      * local scripting layer should not raise events for the
      * WebSocket client.
-
+     *
      * @param h The event handler to use.
      * @param args The event arguments.
      */
     void ProcessLocalEvent(EventHandlerPtr h, zeek::Args args) { DoProcessLocalEvent(h, std::move(args)); }
+
+    /**
+     * Process an error.
+     *
+     * @param code A stringified structured error code not further specified.
+     * @param message A free form message with more context.
+     */
+    void ProcessError(std::string_view code, std::string_view message) { return DoProcessError(code, message); };
 
 private:
     /**
@@ -113,6 +121,14 @@ private:
      * @param args The event arguments.
      */
     virtual void DoProcessLocalEvent(EventHandlerPtr h, zeek::Args args) = 0;
+
+    /**
+     * Hook method for implementing ProcessError().
+     *
+     * @param code A stringified structured error code not further specified.
+     * @param message A free form message with more context.
+     */
+    virtual void DoProcessError(std::string_view code, std::string_view message) = 0;
 };
 
 /**
@@ -122,6 +138,7 @@ class LocalEventHandlingStrategy : public EventHandlingStrategy {
 private:
     bool DoProcessEvent(std::string_view topic, Event e) override;
     void DoProcessLocalEvent(EventHandlerPtr h, zeek::Args args) override;
+    void DoProcessError(std::string_view code, std::string_view message) override;
 };
 
 /**
@@ -284,6 +301,18 @@ protected:
      * @param e The event as cluster::detail::Event.
      */
     bool ProcessEvent(std::string_view topic, detail::Event e);
+
+    /**
+     * An error happened, pass it to the event handling strategy.
+     *
+     * Errors are not necessarily in response to a publish operation, but
+     * can also be raised when receiving messages. E.g. if received data
+     * couldn't be properly parsed.
+     *
+     * @param code A stringified structured error code not further specified.
+     * @param message A free form message with more context.
+     */
+    void ProcessError(std::string_view code, std::string_view message);
 
     /**
      * Process an incoming event message.

zeek/zeek

scripts/base/frameworks/cluster/main.zeek
@@ -667,3 +667,10 @@ event websocket_client_lost(endpoint: EndpointInfo)
 	                endpoint$id, endpoint$network$address, endpoint$network$bound_port);
 	Cluster::log(msg);
 	}
+
+# If a backend reports an error, propagate it via a reporter error message.
+event Cluster::Backend::error(tag: string, message: string)
+	{
+	local msg = fmt("Cluster::Backend::error: %s (%s)", tag, message);
+	Reporter::error(msg);
+	}

src/cluster/Backend.cc
@@ -13,6 +13,7 @@
 #include "zeek/cluster/Manager.h"
 #include "zeek/cluster/OnLoop.h"
 #include "zeek/cluster/Serializer.h"
+#include "zeek/cluster/cluster.bif.h"
 #include "zeek/logging/Manager.h"
 #include "zeek/util.h"
 
@@ -26,6 +27,13 @@ bool detail::LocalEventHandlingStrategy::DoProcessEvent(std::string_view topic, 
 
 void detail::LocalEventHandlingStrategy::DoProcessLocalEvent(EventHandlerPtr h, zeek::Args args) {
     zeek::event_mgr.Enqueue(h, std::move(args));
+}
+
+// Backend errors are raised via a generic Cluster::Backend::error(tag, message) event.
+void detail::LocalEventHandlingStrategy::DoProcessError(std::string_view tag, std::string_view message) {
+    if ( Cluster::Backend::error )
+        zeek::event_mgr.Enqueue(Cluster::Backend::error, zeek::make_intrusive<zeek::StringVal>(tag),
+                                zeek::make_intrusive<zeek::StringVal>(message));
 }
 
 std::optional<zeek::Args> detail::check_args(const zeek::FuncValPtr& handler, zeek::ArgsSpan args) {
@@ -125,6 +133,10 @@ void Backend::EnqueueEvent(EventHandlerPtr h, zeek::Args args) {
 
 bool Backend::ProcessEvent(std::string_view topic, detail::Event e) {
     return event_handling_strategy->ProcessEvent(topic, std::move(e));
+}
+
+void Backend::ProcessError(std::string_view tag, std::string_view message) {
+    return event_handling_strategy->ProcessError(tag, message);
 }
 
 bool Backend::ProcessEventMessage(std::string_view topic, std::string_view format, byte_buffer_span payload) {

src/cluster/Backend.h
@@ -112,11 +112,19 @@ public:
      * When the backend is instantiated for a WebSocket client,
      * local scripting layer should not raise events for the
      * WebSocket client.
-
+     *
      * @param h The event handler to use.
      * @param args The event arguments.
      */
     void ProcessLocalEvent(EventHandlerPtr h, zeek::Args args) { DoProcessLocalEvent(h, std::move(args)); }
+
+    /**
+     * Process an error.
+     *
+     * @param tag A stringified structured error tag not further specified.
+     * @param message A free form message with more context.
+     */
+    void ProcessError(std::string_view tag, std::string_view message) { return DoProcessError(tag, message); };
 
 private:
     /**
@@ -136,6 +144,14 @@ private:
      * @param args The event arguments.
      */
     virtual void DoProcessLocalEvent(EventHandlerPtr h, zeek::Args args) = 0;
+
+    /**
+     * Hook method for implementing ProcessError().
+     *
+     * @param tag A stringified structured error tag not further specified.
+     * @param message A free form message with more context.
+     */
+    virtual void DoProcessError(std::string_view tag, std::string_view message) = 0;
 };
 
 /**
@@ -145,6 +161,7 @@ class LocalEventHandlingStrategy : public EventHandlingStrategy {
 private:
     bool DoProcessEvent(std::string_view topic, Event e) override;
     void DoProcessLocalEvent(EventHandlerPtr h, zeek::Args args) override;
+    void DoProcessError(std::string_view tag, std::string_view message) override;
 };
 
 /**
@@ -321,6 +338,18 @@ protected:
      * @param e The event as cluster::detail::Event.
      */
     bool ProcessEvent(std::string_view topic, detail::Event e);
+
+    /**
+     * An error happened, pass it to the event handling strategy.
+     *
+     * Errors are not necessarily in response to a publish operation, but
+     * can also be raised when receiving messages. E.g. if received data
+     * couldn't be properly parsed.
+     *
+     * @param tag A stringified structured error tag not further specified.
+     * @param message A free form message with more context.
+     */
+    void ProcessError(std::string_view tag, std::string_view message);
 
     /**
      * Process an incoming event message.
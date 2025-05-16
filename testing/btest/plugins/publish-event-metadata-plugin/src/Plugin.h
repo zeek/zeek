79f181a24456cf392f7d89eeb84c0fb92a9d08fa
Arne Welzel <arne.welzel@corelight.com>
2025-05-16 18:01:13 +0200

    Add metadata plugin cluster test

---
+++ testing/btest/plugins/publish-event-metadata-plugin/src/Plugin.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <zeek/plugin/Plugin.h>
+#include <string>
+
+namespace btest::plugin::Demo_PublishEventMetadata {
+
+class Plugin : public zeek::plugin::Plugin {
+protected:
+    zeek::plugin::Configuration Configure() override;
+    void InitPostScript() override;
+
+    bool HookPublishEvent(zeek::cluster::Backend& backend, const std::string& topic,
+                          zeek::cluster::detail::Event& event) override;
+};
+
+extern Plugin plugin;
+
+} // namespace btest::plugin::Demo_PublishEventMetadata

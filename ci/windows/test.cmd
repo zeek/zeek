89837e14a88b7660ab80435941ae0c4bfe0f23f6
Tim Wojtulewicz <tim@corelight.com>
2025-05-28 16:06:18 -0700

    Update Windows CI image to Visual Studio 2022

--- ci/windows/test.cmd
+++ ci/windows/test.cmd
@@ -1,5 +1,5 @@
 :: See build.cmd for documentation on this call.
-call "c:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "c:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 cd build
 

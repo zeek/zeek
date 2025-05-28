295efa7e834446b81eb887ec169a03616ec897c6
Tim Wojtulewicz <tim@corelight.com>
2025-05-28 16:06:18 -0700

    Update Windows CI image to Visual Studio 2022

--- ci/windows/build.cmd
+++ ci/windows/build.cmd
@@ -2,7 +2,7 @@
 :: cmd current shell. This path is hard coded to the one on the CI image, but
 :: can be adjusted if running builds locally. Unfortunately, the initial path
 :: isn't in the environment so we have to hardcode the whole path.
-call "c:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "c:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 mkdir build
 cd build

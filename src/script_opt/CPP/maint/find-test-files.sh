9117ccab12645e6d6cc15de92d993e525f7f0217
Vern Paxson <vern@corelight.com>
2025-05-31 12:50:14 -0700

    fix for more robustly finding BTests to assess for -O gen-C++

--- src/script_opt/CPP/maint/find-test-files.sh
+++ src/script_opt/CPP/maint/find-test-files.sh
@@ -4,7 +4,7 @@ find ../testing/btest -type f |
     grep -v '\.tmp/' |
     xargs grep -E -l '@TEST' |
     xargs grep -E -l '^[ 	]*(event|print)' |
-    xargs grep -E -c 'REQUIRES.*CPP.*((!=.*1)|(==.*0))' |
+    xargs grep -E -c '(REQUIRES.*CPP.*((!=.*1)|(==.*0)))|@TEST-START-NEXT' |
     grep ':0$' |
     sed 's,:0,,' |
     sort

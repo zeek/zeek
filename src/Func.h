a3230b7c814da09c409b4510ab94efac13ecbf2c
Tim Wojtulewicz <tim@corelight.com>
2025-10-07 15:05:22 -0700

    Clean up initialization in Func.h

--- src/Func.h
+++ src/Func.h
@@ -339,20 +339,16 @@ public:
     void Describe(ODesc* d) const override;
 
 protected:
-    BuiltinFunc() {
-        func = nullptr;
-        is_pure = false;
-    }
-
-    built_in_func func;
-    bool is_pure;
+    BuiltinFunc() = default;
+    built_in_func func = nullptr;
+    bool is_pure = false;
 };
 
 extern bool check_built_in_call(BuiltinFunc* f, CallExpr* call);
 
 struct CallInfo {
-    const CallExpr* call;
-    const Func* func;
+    const CallExpr* call = nullptr;
+    const Func* func = nullptr;
     const zeek::Args& args;
 };
 

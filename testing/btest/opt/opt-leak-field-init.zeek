zeek/zeek

commit b5d17f7f49c32e2b87856da01b51cf512c52e0e9

ZAM: Drop some unneeded DeleteManagedTyped() calls
The delete happens by assigning to a slot now. Seems the * and ->
operators added to ZValElement correctly works with the remaining
usages for RawOptField() and RawField().

src/script_opt/ZAM/OPs/constructors.op
@@ -81,8 +81,6 @@ macro	AssignFromRec(rhs)
 		if ( is_managed[i] )
 			{
 			zeek::Ref(rhs_i.ManagedVal());
-			if ( init_i )
-				ZVal::DeleteManagedType(*init_i);
 			}
 		init_i = rhs_i;
 		}
@@ -166,8 +164,6 @@ macro DoManagedRecAssign(lhs, rhs)
 			auto& lhs_i = lhs->RawOptField(lhs_map[i]);
 			auto rhs_i = FieldValWithCheck(rhs, rhs_map[i]);
 			zeek::Ref(rhs_i.ManagedVal());
-			if ( lhs_i )
-				ZVal::DeleteManagedType(*lhs_i);
 			lhs_i = rhs_i;
 			}
 		else
@@ -190,8 +186,6 @@ eval	SetUpRecFieldOps(map)
 		auto& lhs_i = $1->RawOptField(lhs_map[i]);
 		auto rhs_i = FieldValWithCheck($2, rhs_map[i]);
 		zeek::Ref(rhs_i.ManagedVal());
-		if ( lhs_i )
-			ZVal::DeleteManagedType(*lhs_i);
 		lhs_i = rhs_i;
 		}
 

commit e0bb435a9ec3ba61e9e3367c48f9f990e475a28b

ZAM: Fix memory leak in Construct-Known-Record-With-Inits
Commit b5d17f7f49c32e2b87856da01b51cf512c52e0e9 mistakenly removed Unref()
for init_vals pre-generated, but then potentially replaced by values from a
record. Re-instantiate it and add a test that tickled the leak.

This also replaced some autos with the concrete types, even if they
are a mouthful. Mostly to make my poor man's grep-based workflow a bit
easier. Also, given not everything is in .cc files, using an IDE to
figure out types is semi successful (works for the generated code only).

src/script_opt/ZAM/OPs/constructors.op
@@ -76,11 +76,20 @@ macro	AssignFromRec(rhs)
 	auto is_managed = Z_AUX->is_managed;
 	for ( size_t i = 0U; i < n; ++i )
 		{
-		auto rhs_i = FieldValWithCheck(rhs, rhs_map[i]);
-		auto& init_i = init_vals[lhs_map[i]];
+		ZVal rhs_i = FieldValWithCheck(rhs, rhs_map[i]);
+		std::optional<ZVal>& init_i = init_vals[lhs_map[i]];
+		/*
+		 * ConstructRecordPost() used after AssignFromRec() calls the
+		 * optimization specific RecordVal(t, init_vals) constructor
+		 * which adpots managed ZVal references, so need to 1) ref any
+		 * managed fields from rhs and 2) unref managed managed ZVal
+		 * values existing in init_vals created during GenInits().
+		 */
 		if ( is_managed[i] )
 			{
 			zeek::Ref(rhs_i.ManagedVal());
+			if ( init_i.has_value() )
+				zeek::Unref(init_i->ManagedVal());
 			}
 		init_i = rhs_i;
 		}
@@ -111,9 +120,9 @@ eval	auto init_vals = Z_AUX->ToZValVecWithMap(frame);
 	ConstructRecordPost($$)
 
 macro GenInits()
-	auto init_vals = Z_AUX->ToZValVecWithMap(frame);
-	for ( auto& fi : *Z_AUX->field_inits )
-		init_vals[fi.first] = fi.second->Generate();
+	std::vector<std::optional<ZVal>> init_vals = Z_AUX->ToZValVecWithMap(frame);
+	for ( const auto& [idx, field_init] : *Z_AUX->field_inits )
+		init_vals[idx] = field_init->Generate();
 
 op Construct-Known-Record-With-Inits
 class V

src/script_opt/ZAM/ZInstAux.h
@@ -166,7 +166,7 @@ public:
     }
 
     // Returns the elements converted to a vector of ZVal's.
-    const auto& ToZValVec(const ZVal* frame) {
+    const std::vector<std::optional<ZVal>>& ToZValVec(const ZVal* frame) {
         for ( auto i = 0; i < n; ++i )
             zvec[i] = elems[i].ToZVal(frame);
         return zvec;
@@ -175,7 +175,7 @@ public:
     // Same, but using the "map" to determine where to place the values.
     // Returns a non-const value because in this situation other updates
     // may be coming to the vector, too.
-    auto& ToZValVecWithMap(const ZVal* frame) {
+    std::vector<std::optional<ZVal>>& ToZValVecWithMap(const ZVal* frame) {
         for ( auto i = 0; i < n; ++i )
             zvec[map[i]] = elems[i].ToZVal(frame);
         return zvec;

testing/btest/Baseline/opt.opt-leak-field-init/out
@@ -1,0 +1,2 @@
+### BTest baseline data generated by btest-diff. Do not edit. Use "btest -U/-u" to update. Requires BTest >= 0.63.
+[id=[orig_h=1.2.3.4, orig_p=1234/tcp, resp_h=5.6.7.8, resp_p=80/tcp, proto=7, ctx=[]]]

testing/btest/opt/opt-leak-field-init.zeek
@@ -1,0 +1,29 @@
+# @TEST-DOC: ZAM memory leak regression test after ZValElement introduction.
+#
+# @TEST-EXEC: zeek -b -OZAM %INPUT >out
+# @TEST-EXEC: btest-diff out
+
+module Test;
+
+type R: record {
+	id: conn_id;
+};
+
+function x(r: R) {
+	print r;
+}
+
+event zeek_init()
+	{
+	local cid = conn_id(
+		$orig_h=1.2.3.4,
+		$orig_p=1234/tcp,
+		$resp_h=5.6.7.8,
+		$resp_p=80/tcp,
+		$proto=7,
+	);
+	local r = R($id=cid);
+
+	x([$id=r$id]);
+	}
+
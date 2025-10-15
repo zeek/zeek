1e241dbba78a2a3851840c13985090101505230c
Tim Wojtulewicz <tim@corelight.com>
2025-10-15 15:36:41 +0000

    Revert "CI: Pin to python 3.13 on macOS"
    This reverts commit 07e5ad1f8c62e2c87bde7a4695c4a0ee1ebfed9c.

--- ci/macos/prepare.sh
+++ ci/macos/prepare.sh
@@ -7,11 +7,7 @@ set -x
 
 brew update
 brew upgrade cmake
-brew install cppzmq openssl@3 python@3.13 swig bison flex ccache libmaxminddb dnsmasq krb5
-
-# Homebrew doesn't put older versions of python into the PATH by default.
-export PATH=/opt/homebrew/opt/python@3.13/libexec/bin:${PATH}
-echo "export PATH=/opt/homebrew/opt/python@3.13/libexec/bin:${PATH}" >>~/.zshrc
+brew install cppzmq openssl@3 python@3 swig bison flex ccache libmaxminddb dnsmasq krb5
 
 which python3
 python3 --version

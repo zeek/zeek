c87630f59bb2a5098246ff0c2527626a081e14b4
Arne Welzel <arne.welzel@corelight.com>
2025-10-21 10:14:04 +0200

    unknown_ip_transport: Disable endpoint$size and endpoint$state

--- src/packet_analysis/protocol/unknown_ip_transport/UnknownIPSessionAdapter.h
+++ src/packet_analysis/protocol/unknown_ip_transport/UnknownIPSessionAdapter.h
@@ -11,6 +11,24 @@ public:
     UnknownIPSessionAdapter(Connection* conn) : IP::SessionAdapter("Unknown_IP_Transport", conn) {}
 
     void AddExtraAnalyzers(Connection* conn) override;
+
+    void UpdateConnVal(RecordVal* conn_val) override {
+        // Noop - do not install EndpointCallback into conn_val.
+        // This keeps endpoint$state and endpoint$size unset
+        // optionals.
+        for ( Analyzer* a : GetChildren() )
+            a->UpdateConnVal(conn_val);
+    }
+
+    zeek_uint_t GetEndpointSize(bool is_orig) const override {
+        reporter->InternalError("GetEndpointSize() called on UnknownIPSessionAdapter");
+        return 0;
+    }
+
+    zeek_uint_t GetEndpointState(bool is_orig) const override {
+        reporter->InternalError("GetEndpointState() called on UnknownIPSessionAdapter");
+        return 0;
+    }
 };
 
 } // namespace zeek::packet_analysis::UnknownIPTransport

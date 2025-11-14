64a76123d9d19dbedcb0d822ab6f7198c57b5a10
Vern Paxson <vern@corelight.com>
2025-11-13 16:07:36 -0800

    for -O gen-standalone-C++, only compile attributes needed for standalone initialization

--- src/script_opt/CPP/InitsInfo.cc
+++ src/script_opt/CPP/InitsInfo.cc
@@ -332,11 +332,13 @@ AttrInfo::AttrInfo(CPPCompile* _c, const AttrPtr& attr) : CompoundItemInfo(_c) {
             init_cohort = max(init_cohort, gi->InitCohort() + 1);
 
         if ( ! CPPCompile::IsSimpleInitExpr(a_e) ) {
-            gi = c->RegisterInitExpr(a_e);
-            init_cohort = max(init_cohort, gi->InitCohort() + 1);
+            if ( obj_matches_opt_files(a_e) != AnalyzeDecision::SHOULD_NOT ) {
+                gi = c->RegisterInitExpr(a_e);
+                init_cohort = max(init_cohort, gi->InitCohort() + 1);
 
-            vals.emplace_back(Fmt(static_cast<int>(AE_CALL)));
-            vals.emplace_back(Fmt(gi->Offset()));
+                vals.emplace_back(Fmt(static_cast<int>(AE_CALL)));
+                vals.emplace_back(Fmt(gi->Offset()));
+            }
         }
 
         else if ( a_e->Tag() == EXPR_CONST ) {

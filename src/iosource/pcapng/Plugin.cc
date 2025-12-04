ab6ee0488c2ef15c36a6326b7c0a4034843993f1
Tim Wojtulewicz <tim@corelight.com>
2025-12-04 16:30:24 -0700

    Add packet source that uses LightPcapNg to be able to read pcapng files

---
+++ src/iosource/pcapng/Plugin.cc
@@ -0,0 +1,23 @@
+// See the file "COPYING" in the main distribution directory for copyright.
+
+#include "zeek/plugin/Plugin.h"
+
+#include "zeek/iosource/Component.h"
+#include "zeek/iosource/pcapng/Source.h"
+
+namespace zeek::plugin::detail::Zeek_Pcapng {
+
+class Plugin : public plugin::Plugin {
+public:
+    plugin::Configuration Configure() override {
+        AddComponent(new iosource::PktSrcComponent("PcapngReader", "pcapng", iosource::PktSrcComponent::BOTH,
+                                                   iosource::pcapng::Source::Instantiate, {0x0A0D0D0A}));
+
+        plugin::Configuration config;
+        config.name = "Zeek::Pcapng";
+        config.description = "Packet acquisition via pcapng";
+        return config;
+    }
+} plugin;
+
+} // namespace zeek::plugin::detail::Zeek_Pcapng

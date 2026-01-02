c55ad39a93a9d1895521115080eae077f772b9fb
Arne Welzel <arne.welzel@corelight.com>
2026-01-02 20:25:25 +0100

    btest/tools/systemd-generator: Update baselines

--- testing/btest/tools/systemd-generator/test-interface-substitution.sh
+++ testing/btest/tools/systemd-generator/test-interface-substitution.sh
@@ -13,7 +13,7 @@
 #
 # @TEST-EXEC: TEST_DIFF_CANONIFIER=diff-remove-abspath btest-diff ./dir2/zeek-worker@1.service.d/*conf
 # @TEST-EXEC: TEST_DIFF_CANONIFIER=diff-remove-abspath btest-diff ./dir2/zeek-worker@2.service.d/*conf
-# #
+#
 # @TEST-EXEC: mkdir dir3
 # @TEST-EXEC: ${BUILD}/tools/systemd-generator/zeek-systemd-generator --config config3 dir3
 #

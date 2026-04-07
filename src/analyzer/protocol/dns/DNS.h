5dcf1619eda9b507dd6495a4a1da648783d1a1bd
Tim Wojtulewicz <tim@corelight.com>
2026-04-07 11:09:18 -0700

    Add maximum depth for recursive parsing of DNS compression chains
    (cherry picked from commit 94ddba276a6fc65f454547b6f62793aaa720e457)

--- src/analyzer/protocol/dns/DNS.h
+++ src/analyzer/protocol/dns/DNS.h
@@ -364,8 +364,9 @@ private:
     bool ParseAnswer(detail::DNS_MsgInfo* msg, const u_char*& data, int& len, const u_char* start);
 
     u_char* ExtractName(const u_char*& data, int& len, u_char* label, int label_len, const u_char* msg_start,
-                        bool downcase = true);
-    bool ExtractLabel(const u_char*& data, int& len, u_char*& label, int& label_len, const u_char* msg_start);
+                        bool downcase = true, int compression_depth = 0);
+    bool ExtractLabel(const u_char*& data, int& len, u_char*& label, int& label_len, const u_char* msg_start,
+                      int compression_depth = 0);
 
     uint8_t ExtractByte(const u_char*& data, int& len);
     uint16_t ExtractShort(const u_char*& data, int& len);

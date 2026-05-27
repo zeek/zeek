zeek/zeek

commit 460fe24a9a42ee0f24d46353019d89fad9bd868a

Fix clang-tidy cppcoreguidelines-macro-usage findings (macro functions)

src/analyzer/protocol/netbios/NetbiosSSN.cc
@@ -10,14 +10,15 @@
 #include "zeek/analyzer/protocol/netbios/events.bif.h"
 #include "zeek/session/Manager.h"
 
-constexpr double netbios_ssn_session_timeout = 15.0;
+static constexpr double netbios_ssn_session_timeout = 15.0;
 
-#define MAKE_INT16(dest, src)                                                                                          \
-    (dest) = *(src);                                                                                                   \
-    (dest) <<= 8;                                                                                                      \
-    (src)++;                                                                                                           \
-    (dest) |= *(src);                                                                                                  \
-    (src)++;
+static constexpr void MAKE_INT16(uint16_t& dest, const u_char*& src) {
+    dest = *src;
+    dest <= 8;
+    src++;
+    dest |= *src;
+    src++;
+}
 
 namespace zeek::analyzer::netbios_ssn {
 namespace detail {

.clang-tidy
@@ -2,6 +2,8 @@ Checks: [-*,
          bugprone-*,
          performance-*,
 
+         cppcoreguidelines-macro-usage,
+         cppcoreguidelines-misleading-capture-default-by-value,
          cppcoreguidelines-virtual-class-destructor,
 
          # Skipping these temporarily because they are very noisy

src/Expr.cc
@@ -742,6 +742,8 @@ ValPtr BinaryExpr::Fold(Val* v1, Val* v2) const {
         RuntimeErrorWithCallStack("bad type in BinaryExpr::Fold");
 
     switch ( tag ) {
+        // Once we have C++20, these macros can become templated lambdas.
+        // NOLINTBEGIN(cppcoreguidelines-macro-usage)
 #define DO_INT_FOLD(op)                                                                                                \
     if ( is_integral )                                                                                                 \
         i3 = i1 op i2;                                                                                                 \
@@ -771,6 +773,7 @@ ValPtr BinaryExpr::Fold(Val* v1, Val* v2) const {
         i3 = u1 op u2;                                                                                                 \
     else                                                                                                               \
         i3 = d1 op d2;
+            // NOLINTEND(cppcoreguidelines-macro-usage)
 
         case EXPR_ADD:
         case EXPR_ADD_TO: DO_FOLD(+); break;
@@ -892,13 +895,13 @@ ValPtr BinaryExpr::StringFold(Val* v1, Val* v2) const {
 
     switch ( tag ) {
 #undef DO_FOLD
-// NOLINTBEGIN(bugprone-macro-parentheses)
+// NOLINTBEGIN(bugprone-macro-parentheses, cppcoreguidelines-macro-usage)
 #define DO_FOLD(sense)                                                                                                 \
     {                                                                                                                  \
         result = Bstr_cmp(s1, s2) sense 0;                                                                             \
         break;                                                                                                         \
     }
-            // NOLINTEND(bugprone-macro-parentheses)
+            // NOLINTEND(bugprone-macro-parentheses, cppcoreguidelines-macro-usage)
 
         case EXPR_LT: DO_FOLD(<)
         case EXPR_LE: DO_FOLD(<=)

src/Type.cc
@@ -69,6 +69,7 @@ Type::Type(TypeTag t, bool arg_base_type)
       is_network_order(zeek::is_network_order(t)),
       base_type(arg_base_type) {}
 
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define CHECK_TYPE_TAG(tag_type, func_name) CHECK_TAG(tag, tag_type, func_name, type_name)
 
 const TypeList* Type::AsTypeList() const {

src/Val.cc
@@ -53,6 +53,8 @@ Val::~Val() {
 #endif
 }
 
+// NOLINTBEGIN(cppcoreguidelines-macro-usage)
+
 #define CONVERTER(tag, ctype, name)                                                                                    \
     ctype name() {                                                                                                     \
         CHECK_TAG(type->Tag(), tag, "Val::CONVERTER", type_name)                                                       \
@@ -68,6 +70,8 @@ Val::~Val() {
 #define CONVERTERS(tag, ctype, name)                                                                                   \
     CONVERTER(tag, ctype, name)                                                                                        \
     CONST_CONVERTER(tag, ctype, name)
+
+// NOLINTEND(cppcoreguidelines-macro-usage)
 
 CONVERTERS(TYPE_FUNC, FuncVal*, Val::AsFuncVal)
 CONVERTERS(TYPE_FILE, FileVal*, Val::AsFileVal)

src/analyzer/protocol/bittorrent/BitTorrentTracker.cc
@@ -456,7 +456,7 @@ void BitTorrentTracker_Analyzer::ResponseBody(void) {
     }
 }
 
-int BitTorrentTracker_Analyzer::ResponseParseBenc(void) {
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define VIOLATION_IF(expr, msg)                                                                                        \
     {                                                                                                                  \
         if ( expr ) {                                                                                                  \
@@ -466,12 +466,12 @@ int BitTorrentTracker_Analyzer::ResponseParseBenc(void) {
         }                                                                                                              \
     }
 
-#define INC_COUNT                                                                                                      \
-    {                                                                                                                  \
-        unsigned int count = benc_count.back();                                                                        \
-        benc_count.pop_back();                                                                                         \
-        benc_count.push_back(count + 1);                                                                               \
-    }
+int BitTorrentTracker_Analyzer::ResponseParseBenc(void) {
+    auto INC_COUNT = [this]() {
+        unsigned int count = benc_count.back();
+        benc_count.pop_back();
+        benc_count.push_back(count + 1);
+    };
 
     for ( unsigned int len = res_buf_len - (res_buf_pos - res_buf); len; --len, ++res_buf_pos ) {
         switch ( benc_state ) {
@@ -551,7 +551,7 @@ int BitTorrentTracker_Analyzer::ResponseParseBenc(void) {
                         benc_count.pop_back();
 
                         if ( benc_stack.size() )
-                            INC_COUNT
+                            INC_COUNT();
                         else { // benc parsing successful
                             ++res_buf_pos;
                             return 0;
@@ -612,7 +612,7 @@ int BitTorrentTracker_Analyzer::ResponseParseBenc(void) {
                     else
                         VIOLATION_IF(1, "BitTorrentTracker: no valid bencoding")
 
-                    INC_COUNT
+                    INC_COUNT();
                     benc_state = detail::BENC_STATE_EMPTY;
                 }
 
@@ -686,7 +686,7 @@ int BitTorrentTracker_Analyzer::ResponseParseBenc(void) {
                         ++len;
                     }
 
-                    INC_COUNT
+                    INC_COUNT();
                     benc_state = detail::BENC_STATE_EMPTY;
                 }
                 break;

src/analyzer/protocol/login/NVT.cc
@@ -9,7 +9,7 @@
 #include "zeek/analyzer/protocol/login/events.bif.h"
 #include "zeek/analyzer/protocol/tcp/TCP.h"
 
-#define IS_3_BYTE_OPTION(c) ((c) >= 251 && (c) <= 254)
+constexpr bool IS_3_BYTE_OPTION(unsigned int code) { return code >= 251 && code <= 254; }
 
 static constexpr uint8_t TELNET_OPT_SB = 250;
 static constexpr uint8_t TELNET_OPT_SE = 240;
@@ -411,17 +411,13 @@ void NVT_Analyzer::DeliverChunk(int& len, const u_char*& data) {
         if ( binary_mode && c != TELNET_IAC )
             c &= 0x7f;
 
-#define EMIT_LINE                                                                                                      \
-    {                                                                                                                  \
-        buf[offset] = '\0';                                                                                            \
-        ForwardStream(offset, buf, IsOrig());                                                                          \
-        offset = 0;                                                                                                    \
-    }
-
         switch ( c ) {
             case '\r':
-                if ( CRLFAsEOL() & CR_as_EOL )
-                    EMIT_LINE
+                if ( CRLFAsEOL() & CR_as_EOL ) {
+                    buf[offset] = '\0';
+                    ForwardStream(offset, buf, IsOrig());
+                    offset = 0;
+                }
                 else
                     buf[offset++] = c;
                 break;
@@ -433,12 +429,17 @@ void NVT_Analyzer::DeliverChunk(int& len, const u_char*& data) {
                         ;
                     else {
                         --offset; // remove '\r'
-                        EMIT_LINE
+                        buf[offset] = '\0';
+                        ForwardStream(offset, buf, IsOrig());
+                        offset = 0;
                     }
                 }
 
-                else if ( CRLFAsEOL() & LF_as_EOL )
-                    EMIT_LINE
+                else if ( CRLFAsEOL() & LF_as_EOL ) {
+                    buf[offset] = '\0';
+                    ForwardStream(offset, buf, IsOrig());
+                    offset = 0;
+                }
 
                 else {
                     if ( Conn()->FlagEvent(SINGULAR_LF) )

src/analyzer/protocol/ncp/NCP.cc
@@ -9,11 +9,6 @@
 
 using namespace std;
 
-#define xbyte(b, n) (((const u_char*)(b))[n])
-#define extract_uint16(little_endian, bytes)                                                                           \
-    ((little_endian) ? uint16(xbyte(bytes, 0)) | ((uint16(xbyte(bytes, 1))) << 8) :                                    \
-                       uint16(xbyte(bytes, 1)) | ((uint16(xbyte(bytes, 0))) << 8))
-
 namespace zeek::analyzer::ncp {
 namespace detail {
 

commit a48863b8c647caa97f4ada62099aaf2279b1fdb6

Fix typo in operator
This got broken accidentially in
460fe24a9a42ee0f24d46353019d89fad9bd868a.

src/analyzer/protocol/netbios/NetbiosSSN.cc
@@ -12,7 +12,7 @@
 
 static constexpr void MAKE_INT16(uint16_t& dest, const u_char*& src) {
     dest = *src;
-    dest <= 8;
+    dest <<= 8;
     src++;
     dest |= *src;
     src++;
52743e2dbba9f6f063aa24efea131ea858897c20
Arne Welzel <arne.welzel@corelight.com>
2026-05-28 16:37:13 +0200

    demo.py: Remove Receiver, switch to zeekws.Client

--- tools/netcontrol/demo.py
+++ tools/netcontrol/demo.py
@@ -1,22 +1,10 @@
 #!/usr/bin/env python3
 """
-+-------------------+        +------------------------+
-| main thread       |        | NullRouteClient thread |
-+-------------------+        +------------------------+
-|  main()           |        |                        |
-|    Receiver.run() |        |                        |
-|      recv_one()   |        |                        |
-|        ws.recv()  | -----> |  queue.get()           |
-|                   |        |    requests.post()     |
-|                   |        |      zip() results
-|                   |        |        m
-|                   |        |         ws.send()      |
-+-------------------+        +------------------------+
+ws-acld.py - Python side talking to acldng via the netcontrol/pubsub plugin.
 """
 
 import argparse
 import dataclasses
-import json
 import logging
 import os
 import pathlib
@@ -24,9 +12,7 @@ import queue
 import threading
 
 import requests
-import zeek_websocket
-from websockets.sync.client import connect
-from websockets.sync.connection import Connection
+from zeekws.zeekws import Client, RawArg, count, enum
 
 LOGGER = logging.getLogger("acld")
 
@@ -35,15 +21,11 @@ StopRequest = object()
 
 @dataclasses.dataclass
 class PubSubRule:
-    """
-    The type as received from Zeek.
-    """
-
-    ty: str
+    ty: enum
     arg: str
     comment: str
     rule_id: str
-    rule: zeek_websocket.Value
+    rule: RawArg  # unparsed
 
 
 @dataclasses.dataclass
@@ -70,7 +52,7 @@ class NullRouteClient:
         self,
         *,
         job_queue: queue.Queue,
-        ws: Connection,
+        zeek: Client,
         bulk_uri: str,
         namespace: str,
         api_key: str,
@@ -79,7 +61,7 @@ class NullRouteClient:
         request_timeout=10.0,
     ):
         self.job_queue = job_queue
-        self.ws = ws
+        self.zeek = zeek
         self.bulk_uri = bulk_uri
         self.namespace = namespace
         self.api_key = api_key
@@ -94,9 +76,6 @@ class NullRouteClient:
         self.request_timeout = request_timeout
 
         self.logger = logging.getLogger("acld.nullroute_client")
-
-        self.pb = zeek_websocket.ProtocolBinding([])
-        self.pb.outgoing()  # Discard
 
     def do_post(self, op: str, rules: list[PubSubRule]) -> PostResult:
         """ """
@@ -158,20 +137,7 @@ class NullRouteClient:
             event_name = self.result_to_event_name(op, psr_result["status"])
 
             self.logger.debug("reply with %s for %s", event_name, psr.rule)
-
-            args = [
-                zeek_websocket.Value.Count(pubsub_id),
-                psr.rule,
-                "",  ## msg
-            ]
-
-            # This is kind of weird.
-            event = zeek_websocket.Event(event_name, args, metadata=[])
-            self.pb.publish_event(reply_topic, event)
-            msg = self.pb.outgoing()
-            assert msg
-
-            self.ws.send(msg)
+            self.zeek.publish(reply_topic, event_name, [count(pubsub_id), psr.rule, ""])
 
     def process_jobs(self, jobs: list[AddRules | RemoveRules]):
         """
@@ -230,7 +196,9 @@ class NullRouteClient:
 
     def run(self):
         """
-        Runner of the NullRouteClient.
+        Run method of the NullRouteClient
+
+        Consume jobs from the queue until stopped.
         """
         self.logger.info("Running!")
         jobs = []
@@ -251,9 +219,10 @@ class NullRouteClient:
             if (jobs and timed_out) or len(jobs) >= self.batch_jobs:
                 try:
                     self.process_jobs(jobs)
-                except:
+                except Exception as e:
                     LOGGER.exception("Bad")
                     self.stop()
+                    self.zeek.stop(exc_val=e)
                     break
 
                 jobs = []
@@ -262,133 +231,54 @@ class NullRouteClient:
 
     def stop(self):
         """
-        Stop this NullRoute client.
+        Make the NullRoute client's run method stop.
         """
         self.logger.info("Stopping")
         self.stopped = True
         self.job_queue.put_nowait(StopRequest)
 
+    @staticmethod
+    def create(job_queue: queue.Queue, zeek: Client, args: argparse.Namespace):
+        """
+        Create a new NullRouteClient.
 
-class Receiver:
-    """
-    Receive events from Zeek, place them into job_queue where the
-    NullRouteClient will pick them out and reply to Zeek.
+        Tries to find an api_key either in the command-line, environment
+        or a well-known filename /usr/local/etc/acld-ng-apitoken.
 
-    acld-ng <-- HTTP --> broker-acld.py <-- websocket --> Zeek
-    """
+        Args:
+            job_queue: The queue from which to pull jobs.
+            zeek: Used for publishing back to Zeek
+            args: The command-line arguments
+        """
+        api_key: str = ""
+        if args.nullroute_api_key:
+            api_key = args.nullroute_api_key
+        elif "ACLDNG_API_TOKEN" in os.environ:
+            api_key = os.environ["ACLDNG_API_TOKEN"]
+        else:
+            api_key = pathlib.Path(args.nullroute_api_key_filename).read_text().strip()
 
-    def __init__(
-        self,
-        *,
-        ws: Connection,
-        request_topic: str,
-        job_queue: queue.Queue,
-    ):
-        self.ws = ws
-        self.request_topic = request_topic
-        self.pb = zeek_websocket.ProtocolBinding([self.request_topic])
-
-        self.job_queue = job_queue
-
-    def recv_one(self):
-        # This client is driven via events from Zeek.
-        msg = self.ws.recv(decode=False)
-        self.pb.handle_incoming(msg)  # Rust rust rust
-
-        topic_event = self.pb.receive_event()
-        # print("TE", topic_event)
-        if topic_event is not None:
-            topic, event = topic_event
-            name, args = event.name, event.args
-            _ = name
-            reply_topic, pubsub_id, rules = args
-            # LOGGER.debug("event_name=%s reply_topic=%s rules=%s", name, reply_topic, rules)
-            # print("RULES", rules)
-
-            if name == "NetControl::pubsub_add_rules":
-                job = AddRules(
-                    pubsub_id=pubsub_id.value, reply_topic=reply_topic.value, rules=[]
-                )
-            elif name == "NetControl::pubsub_remove_rules":
-                job = RemoveRules(
-                    pubsub_id=pubsub_id.value, reply_topic=reply_topic.value, rules=[]
-                )
-            else:
-                raise ValueError(f"unexpected event {name}")
-
-            # Copy all PubSubRule instances from the received vector.
-            for i, r in enumerate(rules.value):
-                psr = r.as_record(PubSubRule)  # rust rust rust
-                job.rules += [psr]
-
-            # Just put the job into the queue, the NullRouteClient
-            # will pick it up.
-            self.job_queue.put(job)
-
-    def run(self):
-        subscriptions = self.pb.outgoing()
-        assert subscriptions
-        self.ws.send(subscriptions)
-        ack = self.ws.recv()
-        ack = json.loads(ack)
-        if ack.get("type") != "ack" or "endpoint" not in ack:
-            LOGGER.error("Bad ack received from Zeek: %r", ack)
-            raise ValueError(repr(ack))
-
-        LOGGER.info("Got ack %s", ack)
-
-        while True:
-            try:
-                self.recv_one()
-            except KeyboardInterrupt:
-                break
-
-
-def setup_nullroute_client(
-    args, job_queue: queue.Queue, ws: Connection
-) -> NullRouteClient:
-    """
-    This thing isn't all that special? It just does a POST to /nullroute-bulk
-    with a bunch of IPs to block and gets back an array of statuses that match
-    the order of what was passed in?
-
-    This is is an excerpt from the original broker-acld.py code:
-
-        res = client.post(....)
-        res = res.json()
-        for add, r in zip(adds, res["result"])
-            add["result"] = r["result"]
-
-
-    The results should be pushed as events via the WebSocket client
-    back to Zeek using the NetControl::pubsub_rule_added event that
-    receives id, rule and msg.
-    """
-    api_key: str = ""
-    if args.nullroute_api_key:
-        api_key = args.nullroute_api_key
-    elif "ACLDNG_API_TOKEN" in os.environ:
-        api_key = os.environ["ACLDNG_API_TOKEN"]
-    else:
-        try:
-            api_key = (
-                pathlib.Path("/usr/local/etc/acld-ng-apitoken").read_text().strip()
-            )
-        except FileNotFoundError as e:
-            LOGGER.warning("%s", e)
-
-    return NullRouteClient(
-        job_queue=job_queue,
-        ws=ws,
-        bulk_uri=args.nullroute_bulk_uri,
-        namespace=args.nullroute_namespace,
-        api_key=api_key,
-        batch_jobs=args.nullroute_batch_jobs,
-        job_queue_get_timeout=args.nullroute_job_queue_get_timeout,
-    )
+        return NullRouteClient(
+            job_queue=job_queue,
+            zeek=zeek,
+            bulk_uri=args.nullroute_bulk_uri,
+            namespace=args.nullroute_namespace,
+            api_key=api_key,
+            batch_jobs=args.nullroute_batch_jobs,
+            job_queue_get_timeout=args.nullroute_job_queue_get_timeout,
+        )
 
 
 def main():
+    """
+    Entry point.
+
+    Connects to Zeek using zeekws.Client, spawns the NullRouteClient
+    thread, starts consuming events until stopped, places "jobs" into
+    the job_queue for the NullRouteClient to pick up. NullRouteClient
+    uses zeekws.Client.publish() to send the result of the jobs back
+    to Zeek.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--log-level", default="INFO")
     parser.add_argument(
@@ -407,7 +297,11 @@ def main():
     parser.add_argument(
         "--nullroute-bulk-uri", default="http://localhost:8080/nullroute-bulk"
     )
+
     parser.add_argument("--nullroute-api-key", default="")
+    parser.add_argument(
+        "--nullroute-api-key-filename", default="/usr/local/etc/acld-ng-apitoken"
+    )
     parser.add_argument("--nullroute-namespace", default="development")
     parser.add_argument("--nullroute-job-queue-get-timeout", type=float, default=0.01)
     parser.add_argument("--nullroute-batch-jobs", type=int, default=10)
@@ -420,25 +314,39 @@ def main():
     nullroute_client: NullRouteClient | None = None
     nullroute_thread: threading.Thread | None = None
 
-    try:
-        with connect(args.ws_uri, open_timeout=args.ws_open_timeout) as ws:
-            LOGGER.info("Connected...")
-            nullroute_client = setup_nullroute_client(args, job_queue, ws)
-            nullroute_thread = threading.Thread(target=nullroute_client.run)
-            nullroute_thread.start()
+    with Client(args.ws_uri, topics=[args.request_topic]) as zeek:
+        """
+        Zeek event handlers enqueue jobs to NullRouteClient.
+        """
 
-            receiver = Receiver(
-                ws=ws, request_topic=args.request_topic, job_queue=job_queue
-            )
-            receiver.run()
-    finally:
+        @zeek.on("NetControl::pubsub_add_rules")
+        def add_rules(reply_topic: str, pubsub_id: count, rules: list[PubSubRule]):
+            job = AddRules(pubsub_id=pubsub_id, reply_topic=reply_topic, rules=rules)
+            job_queue.put(job)
+
+        @zeek.on("NetControl::pubsub_remove_rules")
+        def remove_rules(reply_topic: str, pubsub_id: count, rules: list[PubSubRule]):
+            job = RemoveRules(pubsub_id=pubsub_id, reply_topic=reply_topic, rules=rules)
+            job_queue.put(job)
+
+        LOGGER.info("Starting NullrouteClientT thread ...")
+        nullroute_client = NullRouteClient.create(job_queue, zeek, args)
+        nullroute_thread = threading.Thread(target=nullroute_client.run)
+        nullroute_thread.start()
+
         try:
-            if nullroute_client is not None:
+            zeek.consume()
+        except KeyboardInterrupt:
+            LOGGER.info("Interrupted")
+        finally:
+            # Shutdown
+            try:
                 nullroute_client.stop()
-                assert nullroute_thread is not None
                 nullroute_thread.join()
-        except Exception as e:
-            LOGGER.exception("Exception shutting down nullroute_client: %s", e)
+            except Exception as e:
+                LOGGER.exception("Exception shutting down nullroute_client: %s", e)
+
+    LOGGER.info("Done")
 
 
 if __name__ == "__main__":

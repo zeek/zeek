18182eb3d7bd52087567fc5dc363a4fc18176bf9
Tim Wojtulewicz <tim@corelight.com>
2026-06-04 14:03:20 -0700

    Add usage of ZEEK_IS_INTERNAL_JOB to control benchmarks/coverage

--- ci/upload-coverage.sh
+++ ci/upload-coverage.sh
@@ -1,13 +1,11 @@
 #! /usr/bin/env bash
 
-# The ZEEK_COVERALLS_REPO_TOKEN environment variable must exist
-# for this script to work correctly. On Cirrus, this is provided
-# via the secured variables.
+# The ZEEK_COVERALLS_REPO_TOKEN environment variable must exist for this script to work
+# correctly. On Circle, this is provided via the Zeek context.
 
-# Only do this on the master branch to avoid having a ton of data
-# in Coveralls.
-if [ "${CIRRUS_REPO_FULL_NAME}" != "zeek/zeek" ]; then
-    echo "Coverage upload skipped for non-zeek repo"
+# Only do this on the main repo to avoid having a ton of data in Coveralls.
+if [ ${ZEEK_IS_INTERNAL_JOB:-0} -ne 1 ]; then
+    echo "Coverage upload skipped for jobs from forks"
     exit 0
 fi
 

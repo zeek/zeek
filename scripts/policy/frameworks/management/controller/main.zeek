zeek/zeek

commit 0c0769b1b217adef3b1c9c8681e8b097628f5cec

Support multiple instances per host addr in auto metrics generation

scripts/policy/frameworks/management/controller/main.zeek
@@ -414,28 +414,34 @@ function config_assign_metrics_ports(config: Management::Configuration)
 		[Supervisor::WORKER] = 3,
 	};
 
-        local instance_metrics_start_port: table[string] of count;
-        local instance_ports_set: table[string] of set[count];
+	local instance_metrics_start_port: table[addr] of count;
+	local instance_ports_set: table[addr] of set[count];
+	local instance_addr_lookup: table[string] of addr;
 	local node: Management::Node;
+	local node_addr: addr;
 
 	# Pre-populate agents ports, if we have them:
 	for ( inst in config$instances )
 		{
-		instance_metrics_start_port[inst$name] = port_to_count(Management::Controller::auto_assign_metrics_start_port);
-		instance_ports_set[inst$name] = {};
+		# build instance name -> addr lookup table
+		instance_addr_lookup[inst$name] = inst$host;
+
+		instance_metrics_start_port[inst$host] = port_to_count(Management::Controller::auto_assign_metrics_start_port);
+		instance_ports_set[inst$host] = {};
 		if ( inst?$listen_port )
-			add instance_ports_set[inst$name][port_to_count(inst$listen_port)];
+			add instance_ports_set[inst$host][port_to_count(inst$listen_port)];
 		}
 
 	# Pre-populate nodes with pre-defined metrics ports, as well
 	# as their Broker ports:
 	for ( node in config$nodes )
+		node_addr = instance_addr_lookup[node$instance];
 		{
 		if ( node?$p )
-			add instance_ports_set[node$instance][port_to_count(node$p)];
+			add instance_ports_set[node_addr][port_to_count(node$p)];
 		if ( node?$metrics_port )
 			{
-			add instance_ports_set[node$instance][port_to_count(node$metrics_port)];
+			add instance_ports_set[node_addr][port_to_count(node$metrics_port)];
 			add new_nodes[node];
 			}
 		}
@@ -467,17 +473,18 @@ function config_assign_metrics_ports(config: Management::Configuration)
 	for ( i in nodes )
 		{
 		node = nodes[i];
+		node_addr = instance_addr_lookup[node$instance];
 
 		# Find next available port ...
-		while ( instance_metrics_start_port[node$instance] in instance_ports_set[node$instance] )
-			++instance_metrics_start_port[node$instance];
+		while ( instance_metrics_start_port[node_addr] in instance_ports_set[node_addr] )
+			++instance_metrics_start_port[node_addr];
 
-		node$metrics_port = count_to_port(instance_metrics_start_port[node$instance], tcp);
+		node$metrics_port = count_to_port(instance_metrics_start_port[node_addr], tcp);
 		add new_nodes[node];
-		add instance_ports_set[node$instance][instance_metrics_start_port[node$instance]];
+		add instance_ports_set[node_addr][instance_metrics_start_port[node_addr]];
 
 		# ... and consume it.
-		++instance_metrics_start_port[node$instance];
+		++instance_metrics_start_port[node_addr];
 		}
 
 	config$nodes = new_nodes;

commit a5d894552131aa63a42e1ef63b0f7a4e6d056dc4

Management framework: fix regression in metrics-port collision avoidance
A bug was introduced in 0c0769b1b2, which added a statement
before the opening braces of the for loop.

This means that most of the ports were never recorded in
instance_ports_set, which can lead to collisions.

Found with the help of Claude Opus 4.7.

scripts/policy/frameworks/management/controller/main.zeek
@@ -418,8 +418,8 @@ function config_assign_metrics_ports(config: Management::Configuration)
 	# Pre-populate nodes with pre-defined metrics ports, as well
 	# as their Broker ports:
 	for ( node in config$nodes )
-		node_addr = instance_addr_lookup[node$instance];
 		{
+		node_addr = instance_addr_lookup[node$instance];
 		if ( node?$p )
 			add instance_ports_set[node_addr][node$p as count];
 		if ( node?$metrics_port )
baf8057b5b4f8f104a1e9ac0abdae7ea8b407ddd
Kevin Harrington <3482852+ke5C2Fin@users.noreply.github.com>
2026-06-13 07:17:24 -0600

    add libressl version check

--- src/OpaqueVal.cc
+++ src/OpaqueVal.cc
@@ -27,7 +27,7 @@
 #include "zeek/probabilistic/BloomFilter.h"
 #include "zeek/probabilistic/CardinalityCounter.h"
 
-#if ( OPENSSL_VERSION_NUMBER < 0x10100000L ) || defined(LIBRESSL_VERSION_NUMBER)
+#if ( OPENSSL_VERSION_NUMBER < 0x10100000L ) || (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x30900000L)
 inline void* EVP_MD_CTX_md_data(const EVP_MD_CTX* ctx) { return ctx->md_data; }
 #endif
 

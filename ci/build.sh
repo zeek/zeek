33cc162e5748780b6a81f94cc41675c2499496fb
Tim Wojtulewicz <tim@corelight.com>
2026-06-17 22:32:14 +0000

    Remove Cirrus config, migrate GH CI config and CI script changes

--- ci/build.sh
+++ ci/build.sh
@@ -1,26 +1,31 @@
 #! /usr/bin/env bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-. ${SCRIPT_DIR}/common.sh
-
-set -e
 set -x
 
-if [[ "${CIRRUS_OS}" == "darwin" ]]; then
+# NOTE: We do not use `nproc` since on some platforms it needs to be explicitly
+# installed while `getconf` is POSIX.
+ZEEK_CI_CPUS=${ZEEK_CI_CPUS:-$(getconf _NPROCESSORS_ONLN)}
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+set -e
+
+# TODO: Is this necessary anymore?
+if [[ "${ZEEK_CI_RUNNER_OS}" == "macos" ]]; then
     # Starting with Monterey & Xcode 13.1 we need to help it find OpenSSL
     if [ -d /usr/local/opt/openssl@1.1/lib/pkgconfig ]; then
         export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/opt/openssl@1.1/lib/pkgconfig
     fi
 fi
 
-if [[ "${ZEEK_CI_CREATE_ARTIFACT}" != "1" ]]; then
+if [[ "${ZEEK_CI_CREATE_INSTALL_TARBALL}" != "1" ]]; then
     ./configure ${ZEEK_CI_CONFIGURE_FLAGS} ${ZEEK_CI_CONFIGURE_FLAGS_EXTRA}
     cd build
     make -j ${ZEEK_CI_CPUS}
 else
-    ./configure ${ZEEK_CI_CONFIGURE_FLAGS} ${ZEEK_CI_CONFIGURE_FLAGS_EXTRA} --prefix=${CIRRUS_WORKING_DIR}/install
+    ./configure ${ZEEK_CI_CONFIGURE_FLAGS} ${ZEEK_CI_CONFIGURE_FLAGS_EXTRA} --prefix=${ZEEK_CI_WORKING_DIR}/install
     cd build
     make -j ${ZEEK_CI_CPUS} install
-    cd ..
-    tar -czf ${CIRRUS_WORKING_DIR}/build.tgz ${CIRRUS_WORKING_DIR}/install
+    cd ${ZEEK_CI_WORKING_DIR}
+    tar -czf ${ZEEK_CI_WORKING_DIR}/install.tgz install
 fi

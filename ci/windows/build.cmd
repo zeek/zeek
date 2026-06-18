1c7c8a444cb0f2fd14a8a81b793147ebc55e6291
Tim Wojtulewicz <tim@corelight.com>
2026-06-18 12:04:49 -0700

    Update Windows CI scripts to match master
    This disables running btests since they haven't been fixed on the
    release branch.

--- ci/windows/build.cmd
+++ ci/windows/build.cmd
@@ -2,10 +2,19 @@
 :: cmd current shell. This path is hard coded to the one on the CI image, but
 :: can be adjusted if running builds locally. Unfortunately, the initial path
 :: isn't in the environment so we have to hardcode the whole path.
-call "c:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-mkdir build
-cd build
+if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
 
-cmake.exe .. -DCMAKE_BUILD_TYPE=release -DVCPKG_TARGET_TRIPLET="x64-windows-static" -DENABLE_ZEEK_UNIT_TESTS=yes -G Ninja
-cmake.exe --build .
+:: vcvarsall always sets this to a value, so we need to reset it so that the
+:: remainder of the commands can set it if needed.
+set ERRORLEVEL=
+
+:: Make sure that long paths are enabled. Otherwise ccache will probably complain about it.
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
+reg query "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled
+
+:: configure.cmd uses a Ninja generator, so this should use the maximum number of CPUs
+:: without -j
+cmake.exe --build build
+if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
